07b05090657a053ceab52f23436f06bdcfa792d5
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2022-04-27 22:31:16 -0500

    Removed @zxing/browser and just used @zxing/library since it allows stopping the camera and everything seems to work just fine without it. Can now scan a barcode and place the value on the page.

--- src/components/Barcode.svelte
+++ src/components/Barcode.svelte
@@ -1,5 +1,5 @@
 <script>
-  import {BrowserQRCodeReader, BrowserMultiFormatReader} from '@zxing/browser';
+  import {BrowserMultiFormatReader} from '@zxing/library';
 
   let selected;
   let code = 'none yet';
@@ -9,21 +9,27 @@
   async function scan() {
     console.log('Selected device ID: ' + selected.deviceId);
 
+    //codeReader.decodeFromVideoDevice(selected.deviceId, 'scanner', ((result) => {
+    //  if(result) {
+    //    code = result.getText();
+    //  }
+    //}));
     codeReader.decodeOnceFromVideoDevice(selected.deviceId, 'scanner').then((result) => {
       if(result) {
         code = result.getText();
+      codeReader.reset();
       }
     });
   }
 
-  function cancel() {
-
+  async function cancel() {
+      codeReader.reset();
   }
 
 </script>
 
 <div class="scanner">
-  {#await BrowserMultiFormatReader.listVideoInputDevices()}
+  {#await codeReader.listVideoInputDevices()}
     <p>..waiting</p>
   {:then inputs}
     <select id="inputs" name="inputs" bind:value={selected} on:change="{() => scan()}">
@@ -32,29 +38,37 @@
       {/each}
     </select>
 
-  <button on:click={scan}>Scan</button>
-  <button on:click={cancel}>Stop</button>
-  <p>Code value: {code}</p>
+  <button class='scan_button' on:click={scan}>Scan</button>
+  <button class='cancel_button' on:click={cancel}>Stop</button>
+  <p class='value'>{code}</p>
   {:catch error}
     <p class=".error">oops!</p>
   {/await}
-  <video id="scanner"></video>
+  <video id="scanner" name="scanner"></video>
 </div>
 <style>
   .scanner {
-    display: flex;
-    flex-flow: column wrap;
-    background: grey;
+    width: 75%;
+    margin: 0 auto;
+    background: #d4cece;
     padding: 15px 20px;
-    justify-content: center;
-    align-items: center;
+    display: block;
+    text-align: center;
   }
-  #inputs {
-    border: black;
-    text-transform: uppercase;
+  select {
+    border: solid 1px grey;
+  }
+  button {
+    padding: 5px 10px;
+  }
+  .value {
+    color: #16bdbd;
+    font-size: 2rem;
   }
   video {
-
+    display: block;
+    min-width: 75%;
+    margin: 0 auto;
   }
   .error {
     color: red;
