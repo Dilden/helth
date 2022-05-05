3d905c84080be7cfe192072d9908d32e0e05ec3d
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2022-05-04 20:11:40 -0500

    Fixing navigation and styles with bootstrap navbar component

---
+++ src/components/Navigation.svelte
@@ -0,0 +1,35 @@
+<script>
+  import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink
+  } from 'sveltestrap/src';
+  import Helth from '../components/Helth.svelte';
+
+  let isOpen = false;
+  const toggle = () => (isOpen = !isOpen);
+</script>
+
+<Navbar color="dark" dark>
+    <NavbarBrand class="me-auto" href="/">
+        <Helth />
+    </NavbarBrand>
+    <NavbarToggler on:click={toggle} class="me-2" />
+    <Collapse {isOpen} navbar>
+        <Nav navbar class="sticky-top">
+            <NavItem>
+                <NavLink href="/">Today</NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink href="/">History</NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink href="/">Profile</NavLink>
+            </NavItem>
+        </Nav>
+    </Collapse>
+</Navbar>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,36 +1,11 @@
 <script>
-    import Helth from '../components/Helth.svelte';
-    import {
-        Collapse,
-        Navbar,
-        NavbarToggler,
-        NavbarBrand,
-        Nav,
-        NavItem,
-        NavLink
-    } from 'sveltestrap/src';
-
-    let isOpen = false;
-    const toggle = () => (isOpen = !isOpen);
+    import Navigation from '../components/Navigation.svelte';
 </script>
+
 <header>
-    <Navbar color="dark">
-        <NavbarBrand class="me-auto">
-            <Helth />
-        </NavbarBrand>
-        <NavbarToggler on:click={toggle} class="me-2"></NavbarToggler>
-        <Collapse {isOpen} navbar>
-            <Nav>
-                <NavItem>
-                    <NavLink href="#components/">Components</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="https://github.com/bestguy/sveltestrap">GitHub</NavLink>
-                </NavItem>
-            </Nav>
-        </Collapse>
-    </Navbar>
+    <Navigation />
 </header>
+
 <slot>
 
 </slot>
@@ -39,8 +14,6 @@
     header {
         background-color: #34474c;
         position: sticky;
-        display: flex;
-        flex-direction: row;
         box-shadow: 0 0 10px black;
     }
 </style>
