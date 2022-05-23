10930ca7a51a9a27cdadeee5b1aa1767744c2f5d
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2022-05-23 15:12:14 -0500

    added limitStore + goalStore into local storage

--- src/stores/local.js
+++ src/stores/local.js
@@ -1,5 +1,5 @@
 import { browser } from '$app/env';
-import { writable, readable } from 'svelte/store';
+import { writable } from 'svelte/store';
 import { thePast } from '../utils/dates';
 
 let currentDay, defaultDay;
@@ -10,10 +10,19 @@ currentDay = defaultDay = {
     salt: 0
 };
 let history = [];
+let goals = {
+    water: 0
+};
+let limits = {
+    calories: 2000,
+    salt: 1500
+};
 
 if (browser) {
     // get local 'today' if it exists; if it doesn't get empty day object
     currentDay = JSON.parse(localStorage.getItem('today')) || defaultDay;
+    goals = JSON.parse(localStorage.getItem('goals')) || goals;
+    limits = JSON.parse(localStorage.getItem('limits')) || limits;
 
     // get local 'history' if it exists; otherwise empty array
     history = JSON.parse(localStorage.getItem('history') || '[]');
@@ -30,12 +39,20 @@ if (browser) {
 
 // set today store to current day
 export const todayStore = writable(currentDay);
+export const goalStore = writable(goals);
+export const limitStore = writable(limits);
 //export const historyStore = readable(history);
 
 if (browser) {
     // update local 'today ' storage if values change
     todayStore.subscribe((value) =>
         localStorage.setItem('today', JSON.stringify(value))
+    );
+    goalStore.subscribe((value) =>
+        localStorage.setItem('goals', JSON.stringify(value))
+    );
+    limitStore.subscribe((value) =>
+        localStorage.setItem('limits', JSON.stringify(value))
     );
     localStorage.setItem('history', JSON.stringify(history));
 }
