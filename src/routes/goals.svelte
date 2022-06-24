5fa5836a109642eee80e658ecf2c36b91cbe6cf4
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2022-06-24 12:49:06 -0500

    mobile friendly flow for counters on goals page

--- src/routes/goals.svelte
+++ src/routes/goals.svelte
@@ -6,18 +6,20 @@
 
 <h2>goals 🥇</h2>
 <div class="container">
-    <div class="item">
+    <div class="wrapper">
+      <div class="item">
         <p>Set your water intake goal</p>
         <Water />
-    </div>
-    <div class="item">
+      </div>
+      <div class="item">
         <p>Set your calorie intake limit</p>
         <Calories />
-    </div>
-    <div class="item">
+      </div>
+      <div class="item">
         <p>Set your sodium intake limit (mg)</p>
         <Salt />
-    </div>
+      </div>
+  </div>
 </div>
 
 <style>
@@ -25,9 +27,24 @@
         text-align: center;
     }
     .container {
-        text-align: center;
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
-        grid-template-rows: 1fr 1fr;
+      padding: 0 15px;
+    }
+    .wrapper {
+      display: flex;
+      flex-flow: row wrap;
+      justify-content: center;
+      width: 100%;
+      gap: 15px;
+    }
+    .item {
+      text-align: center;
+      flex: 1 1 auto;
+      max-width: 32%;
+      padding: 0;
+    }
+    @media screen and (max-width: 925px) {
+      .item {
+        max-width: 65%;
+      }
     }
 </style>
