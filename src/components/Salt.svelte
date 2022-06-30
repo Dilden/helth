146fd7b5dfb1ace0213c8ff4c982c9f2dbe7c7eb
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2022-06-30 13:35:44 -0500

    added title text to show units

--- src/components/Salt.svelte
+++ src/components/Salt.svelte
@@ -1,7 +1,7 @@
 <script>
     import Counter from '../components/Counter.svelte';
     import { todayStore } from '../stores/local';
-    let title = '🧂';
+    let title = '🧂 sodium (mg)';
 </script>
 
 <Counter {title} bind:count={$todayStore.salt} />
