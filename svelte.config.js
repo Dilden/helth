2ac25dab9f0c5384c0757e26d77d7fcf3010e397
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2022-07-06 14:48:08 -0500

    basic pwa config

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-node';
 import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,20 +13,23 @@ const config = {
         },
         trailingSlash: 'always',
         vite: {
-            server: {
-                fs: {
-                    allow: ['static']
-                }
-            },
-            resolve: {
-                alias: {
-                    // set an alias so images can be dynamically imported
-                    $static: path.resolve('./static'),
-                    $components: path.resolve('./src/components'),
-                    $utils: path.resolve('./src/utils'),
-                    $stores: path.resolve('./src/stores')
-                }
-            }
+          plugins: [
+            VitePWA({ registerType: 'autoUpdate'})
+          ],
+          server: {
+              fs: {
+                  allow: ['static']
+              }
+          },
+          resolve: {
+              alias: {
+                  // set an alias so images can be dynamically imported
+                  $static: path.resolve('./static'),
+                  $components: path.resolve('./src/components'),
+                  $utils: path.resolve('./src/utils'),
+                  $stores: path.resolve('./src/stores')
+              }
+          }
         }
     }
 };
