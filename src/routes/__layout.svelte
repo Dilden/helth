abb358e7557a5ed3cb956b9599e133a7460ab27d
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2022-07-07 14:25:04 -0500

    added reload prompt to PWA. Issue with vite-plugin-pwa not supporting latest release of SvelteKit hasn't been addressed yet. See https://github.com/antfu/vite-plugin-pwa/issues/324 for more info

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,25 @@
 <script>
-    import Navigation from '$components/Nav/Navigation.svelte';
+  import Navigation from '$components/Nav/Navigation.svelte';
+  import { onMount } from 'svelte'
+	import { browser, dev } from '$app/env'
+
+	const loadRPC = !dev && browser
+	let ReloadPrompt
+	onMount(async () => {
+		loadRPC && (ReloadPrompt = (await import('$components/ReloadPrompt.svelte')).default)
+	})
 </script>
+
+<svelte:head>
+	{#if !dev}
+		<link rel="manifest" href="/_app/immutable/manifest.webmanifest">
+	{/if}
+</svelte:head>
 
 <Navigation />
 
 <slot />
+
+{#if ReloadPrompt}
+	<svelte:component this={ReloadPrompt} />
+{/if}
