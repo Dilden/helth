b19b91e217e265df49948daeedfdb89d82980b61
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2022-07-07 14:56:25 -0500

    Flexbox containing content and creating sticky footer

---
+++ src/components/Nav/Footer.svelte
@@ -0,0 +1,14 @@
+<footer>
+  <p>
+    made w/💕 by <a href="https://www.closingtags.com">closingtags</a> & powered by <a href="https://kit.svelte.dev">SvelteKit</a>
+  </p>
+</footer>
+
+<style>
+  footer {
+    text-align: center;
+  }
+  p {
+    padding: 30px 0 15px;
+  }
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,28 @@
 <script>
-    import Navigation from '$components/Nav/Navigation.svelte';
+  import Navigation from '$components/Nav/Navigation.svelte';
+  import Footer from '$components/Nav/Footer.svelte';
 </script>
 
-<Navigation />
+<div class='main'>
+  <Navigation />
+  <div class='content'>
+    <slot />
+  </div>
+  <div class='footer'>
+    <Footer />
+  </div>
+</div>
 
-<slot />
+<style>
+  .main {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+  .content {
+    flex: 1 0 auto;
+  }
+  .footer {
+    flex-shrink: 0;
+  }
+</style>
