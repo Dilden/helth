17092ab27305490bea4f7c2ffa0a095649b00636
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2022-07-07 15:20:10 -0500

    Breaking graphs out into 'chart' components for ease of reproduction.

---
+++ src/components/charts/CalorieChart.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Chart from '$components/charts/Chart.svelte';
+  let data = [12, 19, 3, 5, 2, 30];
+  let labels = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Pickle'];
+</script>
+
+<Chart chartType="line" data={data} labels={labels} title='Calories'/>
--- src/routes/history.svelte
+++ src/routes/history.svelte
@@ -1,51 +1,10 @@
 <script>
-    import { onMount } from 'svelte';
-    import { Chart } from 'chart.js/auto/auto.js';
-
-    let portfolio;
-    onMount(()=> {
-        const ctx = portfolio.getContext('2d');
-        // Initialize chart using default config set
-        const myChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-    });
+  import CalorieChart from '$components/charts/CalorieChart.svelte';
 </script>
 
 <h2>history 📈</h2>
 <div class="container">
-    <canvas bind:this={portfolio} width={400} height={400} />
+  <CalorieChart />
 </div>
 
 <style>
