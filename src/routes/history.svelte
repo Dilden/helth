317ff850fe79cf81373d7e5adaa137290e96d579
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2022-07-08 16:36:27 -0500

    include new water chart

--- src/routes/history.svelte
+++ src/routes/history.svelte
@@ -1,20 +1,30 @@
 <script>
   import CalorieChart from '$components/charts/CalorieChart.svelte';
+  import WaterChart from '$components/charts/WaterChart.svelte';
 </script>
 
 <h2>history 📈</h2>
-<div class="container">
-  <CalorieChart />
+<div class='charts'>
+  <div class='container'>
+    <CalorieChart />
+  </div>
+  <div class='container'>
+    <WaterChart />
+  </div>
 </div>
 
 <style>
     h2 {
         text-align: center;
     }
+    .charts {
+      text-align: center;
+    }
     .container {
         width: 500px;
         height: auto;
         text-align: center;
         margin: 0 auto;
+        display: inline-block;
     }
 </style>
