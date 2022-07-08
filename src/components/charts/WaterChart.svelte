73415724547b089cdfc6a552e828d317a96c9336
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2022-07-08 16:35:54 -0500

    Water chart component

---
+++ src/components/charts/WaterChart.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Chart from '$components/charts/Chart.svelte';
+  import { historyStore } from '$stores/local';
+
+  let data = $historyStore.map(el => el.water);
+  let labels = $historyStore.map(el => new Date( el.date ).getDate() + '/' + new Date( el.date ).getMonth() + '/' + new Date( el.date ).getFullYear());
+</script>
+
+<Chart chartType="line" data={data} labels={labels} title='Water' color="#2417fc"/>
