b77c5e577a9ff8509192080c653442ef692f53d4
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2022-07-08 16:35:21 -0500

    Chart component data, labels, title, and color are now extendable by decendants

--- src/components/charts/Chart.svelte
+++ src/components/charts/Chart.svelte
@@ -7,6 +7,7 @@
   export let data = [];
   export let labels = [];
   export let title = '';
+  export let color = '#000000';
 
   onMount(()=> {
       const ctx = chart.getContext('2d');
@@ -17,22 +18,8 @@
               datasets: [{
                   label: title,
                   data: data,
-                  backgroundColor: [
-                      'rgba(255, 99, 132, 0.2)',
-                      'rgba(54, 162, 235, 0.2)',
-                      'rgba(255, 206, 86, 0.2)',
-                      'rgba(75, 192, 192, 0.2)',
-                      'rgba(153, 102, 255, 0.2)',
-                      'rgba(255, 159, 64, 0.2)'
-                  ],
-                  borderColor: [
-                      'rgba(255, 99, 132, 1)',
-                      'rgba(54, 162, 235, 1)',
-                      'rgba(255, 206, 86, 1)',
-                      'rgba(75, 192, 192, 1)',
-                      'rgba(153, 102, 255, 1)',
-                      'rgba(255, 159, 64, 1)'
-                  ],
+                  backgroundColor: color,
+                  borderColor: color,
                   borderWidth: 1
               }]
           },
