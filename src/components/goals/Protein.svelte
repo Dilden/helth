44f0729c59b9dbf9b7e69d26405d9d16935b63b2
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2022-07-28 14:44:51 -0500

    New protein goal

---
+++ src/components/goals/Protein.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Counter from '$components/counts/Counter.svelte';
+    import { goalStore } from '$stores/local.js';
+    let title = '🍗 protein (g)';
+</script>
+
+<Counter {title} bind:count={$goalStore.protein} />
