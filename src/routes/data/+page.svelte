006e31a6e6c5c2724a3b3071f8dbf16b418357c5
dylan <dylan.hildenbrand@gmail.com>
2022-10-07 15:01:24 -0500

    New components for import/export of data Added data route Show data route in menu Tweaked footer

---
+++ src/routes/data/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  import ExportData from '$components/data/ExportData.svelte';
+  import ImportData from '$components/data/ImportData.svelte';
+</script>
+
+<div class="container">
+  <ExportData />
+  <ImportData />
+</div>
+
+<style>
+  .container {
+    padding: 30px;
+    text-align: center;
+  }
+</style>
