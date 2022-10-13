36f9306d233e5e83a27b8c0f7ca8aab4fa5de6c2
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2022-10-13 11:38:02 -0500

    Added tables for settings, limits, goals, and changed journal table PK to be unique date. Add day sets the day with zeroed out hours, min, sec, ms. When DB is opened, check if most recent day in the table is today, if not, set a new day.

--- src/stores/db.js
+++ src/stores/db.js
@@ -1,21 +1,36 @@
 import { Dexie, liveQuery} from 'dexie';
+import { thePast } from '$utils/dates';
 import { browser } from '$app/environment';
 
 export const db = new Dexie('helthdb');
 
 db.version(1).stores({
-  journal: '++id, $date, water, calories, protein, sodium',
-  settings: '++id, date, water, calories, protein, sodium'
+  journal: 'date, water, calories, protein, sodium',
+  settings: '++id, &name, value', 
+  limits: '++id, &name, value',
+  goals: '++id, &name, value'
+});
+
+db.open().then((db) => {
+
+  // check if today's date is most recent
+  // add empty day if it is not
+  db.journal.orderBy('date').reverse().first()
+  .then(record => {
+    if(!record || thePast(record.date)) {
+      addDay();
+    }
+  })
 });
 
 export async function addDay() {
   try {
     const today = await db.journal.add({
-      date: new Date(),
-      water: 1500,
-      calories: Math.floor(Math.random() * 3500),
-      protein: 15,
-      salt: 1250
+      date: new Date().setHours(0, 0, 0, 0),
+      water: 0,
+      calories: 0,
+      protein: 0,
+      salt: 0
     });
     console.log(`added ${today}`);
   } catch (error) {
@@ -23,7 +38,7 @@ export async function addDay() {
   }
 }
 
-export const getToday = liveQuery(async () => {
+export const getLatestDay = liveQuery(async () => {
   if(browser) {
     return db.journal.orderBy('date').reverse().first();
   }
