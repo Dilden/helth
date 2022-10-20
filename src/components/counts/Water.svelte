f3b632918bbda74e0f7bf225c0ce16db9f4e37c0
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2022-10-20 13:17:15 -0500

    Used IndexedDB based new store 'today' instead of localStorage based store 'todayStore'.

--- src/components/counts/Water.svelte
+++ src/components/counts/Water.svelte
@@ -1,11 +1,19 @@
 <script>
   import Counter from '$components/counts/Counter.svelte';
-  import { todayStore, settingStore, goalStore } from '$stores/local';
+  import { settingStore, goalStore } from '$stores/local';
+  import { today } from '$stores/stores';
+  import Spinner from '$components/Spinner.svelte';
 
   let title = '💧 water (mL)';
   let max = 1000;
-  $: diff = $goalStore.water - $todayStore.water;
+  $: diff = $goalStore.water - $today.water;
   $: diffString = (diff >= 0 ) ? diff + ' remaining' : -diff + ' over goal!';
 </script>
 
-<Counter {title} {max} {diffString} bind:interval={$settingStore.waterInterval} bind:count={$todayStore.water} />
+{#await today.init()}
+  <Spinner />
+{:then}
+  <Counter {title} {max} {diffString} bind:interval={$settingStore.waterInterval} bind:count={$today.water} />
+{:catch error}
+  <p>error</p>
+{/await}
