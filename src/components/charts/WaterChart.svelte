2f0385dec44b038962af10920926ded17f22a7f2
dylan <dylan.hildenbrand@gmail.com>
2022-10-26 21:10:43 -0500

    Working on pulling history + goal stores into charts

--- src/components/charts/WaterChart.svelte
+++ src/components/charts/WaterChart.svelte
@@ -1,25 +1,39 @@
 <script>
+  import { onMount } from 'svelte';
+  import { history, goals } from '$stores/stores';
   import Chart from '$components/charts/Chart.svelte';
-  import { todayStore, historyStore, goalStore } from '$stores/local';
+  import Spinner from '$components/Spinner.svelte';
 
-  let data = [
-      {
-        label: 'Water',
-        data: $historyStore.map(el => el.water),
-        backgroundColor: "#2417fc",
-        borderColor: "#2417fc"
-      }
-    ];
+  $: data = [];
+  $: labels = [];
 
-  let labels = $historyStore.map(el => {
-          let date = new Date(el.date);
-          return date.toLocaleDateString();
-      }
-    );
-  // push today onto data + labels
-  data[0].data.push($todayStore.water);
-  labels.push('today');
+  onMount(() => {
+    history.init()
+    .then(() => {
+      data = [
+        {
+          label: 'Water',
+          data: $history.map(el => el.water),
+          backgroundColor: "#2417fc",
+          borderColor: "#2417fc"
+        }
+      ];
+
+      labels = $history.map(el => {
+            let date = new Date(el.date);
+            return date.toLocaleDateString();
+        }
+      );
+    })
+  });
+
 </script>
 
 <h3>Water</h3>
-<Chart chartType="line" goal={$goalStore.water} {data} {labels} unit={"ml"} />
+{#await goals.init()}
+  <Spinner />
+{:then}
+  <Chart chartType="line" goal={$goals.water.value} {data} {labels} unit={"ml"} />
+{:catch error}
+  <p>error</p>
+{/await}
