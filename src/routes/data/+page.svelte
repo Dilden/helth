9636101dda1794896a182b2a09eaca34ba636f5c
dylan <dylan.hildenbrand@gmail.com>
2022-10-27 10:33:34 -0500

    Removed old data export; nobody has been using this app anyways lol

--- src/routes/data/+page.svelte
+++ src/routes/data/+page.svelte
@@ -1,11 +1,9 @@
 <script>
   import ExportData from '$components/data/ExportData.svelte';
-  import ExportDataOld from '$components/data/ExportDataOld.svelte';
   import ImportData from '$components/data/ImportData.svelte';
 </script>
 
 <div class="container">
-  <div><ExportDataOld /></div>
   <div><ExportData /></div>
   <div><ImportData /></div>
 </div>
