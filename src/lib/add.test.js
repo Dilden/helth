4aa6f4233ad47618cdd559941567366fc92aed33
dylan <dylan.hildenbrand@gmail.com>
2023-08-25 16:05:12 -0500

    Add needs an inventory passed to it now.

--- src/lib/add.test.js
+++ src/lib/add.test.js
@@ -2,9 +2,14 @@ import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/svelte';
 import Add from './Add.svelte';
 
+const inventory = [ 
+  { title: 'Big Mac' },
+  { title: 'Whopper' }
+]
+
 describe('Add dialog component', () => {
   it('shows tabs for inventory, recipes, and scan', () => {
-    render(Add);
+    render(Add, {inventory});
 
     const tabs = screen.queryAllByRole('button');
     const tabNames = tabs.map(tab => tab.textContent);
