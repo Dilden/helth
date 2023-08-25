eee79e63673fe97202e0cc83def09e7a87a987b3
dylan <dylan.hildenbrand@gmail.com>
2023-08-25 14:29:02 -0500

    Outlining end-to-end tests for inventory features

--- tests/addDialog.test.js
+++ tests/addDialog.test.js
@@ -7,8 +7,45 @@ const openDialog = async ( page ) => {
 
 test.describe('add items dialog', () => {
 
-  test('inventory is shown by default', async ({ page }) => {
-    openDialog(page);
-    await expect(page.getByText('Saved Items')).toBeVisible();
-  });
+  test.describe('inventory', () => {
+
+    test('inventory is shown by default', async ({ page }) => {
+      openDialog(page);
+      await expect(page.getByText('Saved Items')).toBeVisible();
+    });
+
+    test.skip('scanning item adds it to inventory', async ({ page }) => {
+
+    })
+
+    test.skip('add item in inventory to daily total', async ({ page }) => {
+      openDialog(page);
+      // await page.getByRole('button', {name: '🖉'}).filter()
+    })
+    test.skip('edit item in inventory', async ({ page }) => {
+      openDialog(page);
+      // await page.getByRole('button', {name: '🖉'}).filter()
+    })
+    test.skip('delete item in inventory', async ({ page }) => {
+      openDialog(page);
+      // await page.getByRole('button', {name: '🖉'}).filter()
+    })
+    test.skip('user can manually add an item to inventory', () => {
+       
+    })
+  })
+
+  test.describe('recipes', () => {
+    test.skip('user is able to add an inventory item to a recipe', async ({ page }) => {
+      openDialog(page);
+      // await page.getByRole('button', {name: '🖉'}).filter()
+    })
+
+  })
+  test.describe('scanner', () => {
+    test.skip('scanning a barcode adds it to the inventory', async ({ page }) => {
+      openDialog(page);
+      // await page.getByRole('button', {name: '🖉'}).filter()
+    })
+  })
 })
