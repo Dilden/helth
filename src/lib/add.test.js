85ff3ff9c12d23174184df3ede1c44c36e54b2fc
dylan <dylan.hildenbrand@gmail.com>
2023-08-25 11:29:25 -0500

    moved test away from playwright and to vitest

---
+++ src/lib/add.test.js
@@ -0,0 +1,19 @@
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/svelte';
+import Add from './Add.svelte';
+
+describe('Add dialog component', () => {
+  it('shows tabs for inventory, recipes, and scan', () => {
+    render(Add);
+
+    const tabs = screen.queryAllByRole('button');
+    const tabNames = tabs.map(tab => tab.textContent);
+    expect(tabNames).toEqual(
+      expect.arrayContaining([
+        expect.stringContaining('Inventory'),
+        expect.stringContaining('Recipes'),
+        expect.stringContaining('Scan')
+      ])
+    );
+  })
+})
--- tests/addDialog.test.js
+++ tests/addDialog.test.js
@@ -1,5 +1,4 @@
 import { expect, test } from '@playwright/test';
-// /import { getInventory, addInventory } from '../src/stores/db.js';
 
 const openDialog = async ( page ) => {
   await page.goto('/');
@@ -7,12 +6,6 @@ const openDialog = async ( page ) => {
 }
 
 test.describe('add items dialog', () => {
-  test('add dialog shows inventory, recipes, and scan tabs', async ({ page }) => {
-    openDialog(page);
-    await expect(page.getByRole('button', { name: 'Inventory' })).toBeVisible();
-    await expect(page.getByRole('button', { name: 'Recipes' })).toBeVisible();
-    await expect(page.getByRole('button', { name: 'Scan' })).toBeVisible();
-  });
 
   test('inventory is shown by default', async ({ page }) => {
     openDialog(page);
