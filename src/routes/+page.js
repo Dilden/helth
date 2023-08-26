59b376226012452b55be92be1c1fb0af6013a525
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2023-08-26 16:41:44 -0500

    Pulled inventory data from db.js (dexie) and removed useless test file

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,11 +1,16 @@
+import { getInventory } from '../stores/db.js';
+
 export const ssr = false;
 
 export function load() {
   return {
-    inventory: [
-      {title: 'Coca-Cola'},
-      {title: 'Pepsi'},
-      {title: 'Monster'}
-    ]
+    inventory: getInventory()
   }
+  // return {
+  //   inventory: [
+  //     {title: 'Coca-Cola'},
+  //     {title: 'Pepsi'},
+  //     {title: 'Monster'}
+  //   ]
+  // }
 }
