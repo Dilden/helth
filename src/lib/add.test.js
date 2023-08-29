d1a3c612489654edb73b8a2591564c28de748007
dylan <dylan.hildenbrand@gmail.com>
2023-08-29 11:18:36 -0500

    Pass empty inventory to add component, import fake-indexeddb/auto before tests that use Dexie

--- src/lib/add.test.js
+++ src/lib/add.test.js
@@ -1,3 +1,4 @@
+import 'fake-indexeddb/auto';
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/svelte';
 import Add from './Add.svelte';
@@ -9,7 +10,7 @@ const inventory = [
 
 describe('Add dialog component', () => {
   it('shows tabs for inventory, recipes, and scan', () => {
-    render(Add, {inventory});
+    render(Add, {inventory: []});
 
     const tabs = screen.queryAllByRole('button');
     const tabNames = tabs.map(tab => tab.textContent);
@@ -23,12 +24,7 @@ describe('Add dialog component', () => {
   });
 
   it('accepts inventory data', () => {
-    render(Add, {
-      inventory: [ 
-        { title: 'Big Mac' },
-        { title: 'Whopper' }
-      ]
-    });
+    render(Add, {inventory});
 
     expect(screen.queryByText('Big Mac')).toBeVisible();
     expect(screen.queryByText('Whopper')).toBeVisible();
