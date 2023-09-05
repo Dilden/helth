aaa9c4f57b550a23172191e6dac9fee792eef1f8
dylan <dylan.hildenbrand@gmail.com>
2023-09-05 15:06:08 -0500

    rolling back tests for previous commit

--- src/lib/add.test.js
+++ src/lib/add.test.js
@@ -3,14 +3,9 @@ import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/svelte';
 import Add from './Add.svelte';
 
-const inventory = [ 
-  { title: 'Big Mac' },
-  { title: 'Whopper' }
-]
-
 describe('Add dialog component', () => {
   it('shows tabs for inventory, recipes, and scan', () => {
-    render(Add, {inventory: []});
+    render(Add);
 
     const tabs = screen.queryAllByRole('button');
     const tabNames = tabs.map(tab => tab.textContent);
@@ -22,12 +17,4 @@ describe('Add dialog component', () => {
       ])
     );
   });
-
-  it('accepts inventory data', () => {
-    render(Add, {inventory});
-
-    expect(screen.queryByText('Big Mac')).toBeVisible();
-    expect(screen.queryByText('Whopper')).toBeVisible();
-
-  })
 })
