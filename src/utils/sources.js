e4d1605ef271a7eda212b5f9690ceac69b692711
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2023-09-20 15:54:47 -0500

    More transformations to returned API data

--- src/utils/sources.js
+++ src/utils/sources.js
@@ -2,21 +2,25 @@ import { snake_case } from '$utils/naming.js';
 import { list } from '$utils/nutrients.js'
 
 export const formatSource2 = (data) => {
-
   const nutrientsObj = Object.entries(data.data.product_details.nutrition_labels)
     .reduce((accum, key) => {
       let nutrientName = snake_case(key[0]);
+
       switch(nutrientName) {
         case 'fat':
           nutrientName = 'total_fat';
           break;
+        case 'carbs':
+          nutrientName = 'total_carbohydrate';
+          break;
       }
-
-      accum[nutrientName] = accum[nutrientName] || {};
-      accum[nutrientName].unit = ( key[1].uom ? key[1].uom : list[nutrientName].unit );
-      accum[nutrientName].name = ( list[nutrientName].name );
-      accum[nutrientName].quantity = ( key[1].total_quantity ? String(key[1].total_quantity) : '0' );
       
+      if(nutrientName !== 'serving') {
+        accum[nutrientName] = accum[nutrientName] || {};
+        accum[nutrientName].unit = ( key[1].uom ? key[1].uom : list[nutrientName]?.unit );
+        accum[nutrientName].name = ( list[nutrientName].name );
+        accum[nutrientName].quantity = ( key[1].total_quantity ? String(key[1].total_quantity) : '0' );
+      }
       return accum;
     }, {});
 
