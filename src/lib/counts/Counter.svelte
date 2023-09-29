6fb83816b2d4b42b74958acfa576079f7ec084f7
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2023-09-29 15:41:21 -0500

    Show counter input labels as title instead of H3 heading, change h5 heading to div, give increment interval aria-label prop

--- src/lib/counts/Counter.svelte
+++ src/lib/counts/Counter.svelte
@@ -18,92 +18,95 @@
 </script>
 
 <div class="container">
-  <h3>{title}</h3>
-  <h5>{@html diffString}</h5> 
+  <label for="countValue_{title}" class="countTitle">{title}</label>
+  <div class="diffString">{@html diffString}</div> 
   <div class="controls">
     <button on:click={decrement}> -{interval} </button>
 
-    <label for="countValue">Count value</label>
-    <input id="countValue" bind:value={count} type="number" min="0" />
+    <input id="countValue_{title}" bind:value={count} type="number" min="0" />
 
     <button on:click={increment}> +{interval} </button>
   </div>
-  <label for="interval">interval</label>
-  <input id="interval" type="range" min=1 max={max} bind:value={interval}/>
+  <input id="interval" type="range" min=1 max={max} bind:value={interval} aria-label="{title} Increment & Decrement Interval"/>
 </div>
 
 <style>
-    .container {
-      text-align: center;
-    }
-    .controls {
-      display: -webkit-box;
-      display: -moz-box;
-      display: -ms-flexbox;
-      display: -webkit-flex;
-      display: flex;
-      align-content: center;
-      align-items: center;
-      gap: 0;
-    }
-    input, button {
-      font-size: 1.5em;
-      border: none;
-      margin: auto;
-      padding: 10px;
-      color: #000000;
-      touch-action: manipulation;
-    }
-    label {
-      display: none;
-    }
-    input[type='number'] {
-      -webkit-box-flex: 2 1 auto;
-      -moz-box-flex: 2 1 auto;
-      -webkit-flex: 2 1 auto;
-      -ms-flex: 2 1 auto;
-      -moz-appearance: textfield;
-      appearance: textfield;
-      flex: 2 1 auto;
-      width: 50%;
-      -webkit-border-radius: 0;
-      -moz-border-radius: 0;
-      border-radius: 0;
-      min-width: 75px;
-    }
-    input::-webkit-outer-spin-button,
-    input::-webkit-inner-spin-button {
-      -webkit-appearance: none;
-      margin: 0;
-    }
-    input[type='range'] {
-      width: 90%;
-      font-size: 3em;
-      padding: 0;
-      margin: 10px auto;
-    }
-    button {
-      appearance: none;
-      -webkit-appearance: none;
-      -webkit-box-flex: 1 1 auto;
-      -moz-box-flex: 1 1 auto;
-      -webkit-flex: 1 1 auto;
-      -ms-flex: 1 1 auto;
-      flex: 1 1 auto;
-      background-color: #e9e9ed;
-      transition: all 0.2s ease-in-out;
-    }
-    button:first-child {
-      -webkit-border-radius: 10px 0 0 10px;
-      -moz-border-radius: 10px 0 0 10px;
-      border-radius: 10px 0 0 10px;
-    }
-    button:last-child {
-      -webkit-border-radius: 0 10px 10px 0;
-      -moz-border-radius: 0 10px 10px 0;
-      border-radius: 0 10px 10px 0;
-    }
-    button:hover {
-      background-color: #d0d0d7;
-    }
+  .diffString {
+    font-weight: 500;
+    margin: .5rem;
+  }
+  .countTitle {
+    font-size: calc(1rem * var(--heading-ratio) * var(--heading-ratio));
+    font-weight: 500;
+  }
+  .container {
+    text-align: center;
+  }
+  .controls {
+    display: -webkit-box;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+    align-content: center;
+    align-items: center;
+    gap: 0;
+  }
+  input, button {
+    font-size: 1.5em;
+    border: none;
+    margin: auto;
+    padding: 10px;
+    color: #000000;
+    touch-action: manipulation;
+  }
+  input[type='number'] {
+    -webkit-box-flex: 2 1 auto;
+    -moz-box-flex: 2 1 auto;
+    -webkit-flex: 2 1 auto;
+    -ms-flex: 2 1 auto;
+    -moz-appearance: textfield;
+    appearance: textfield;
+    flex: 2 1 auto;
+    width: 50%;
+    -webkit-border-radius: 0;
+    -moz-border-radius: 0;
+    border-radius: 0;
+    min-width: 75px;
+  }
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+  input[type='range'] {
+    width: 90%;
+    font-size: 3em;
+    padding: 0;
+    margin: 10px auto;
+  }
+  button {
+    appearance: none;
+    -webkit-appearance: none;
+    -webkit-box-flex: 1 1 auto;
+    -moz-box-flex: 1 1 auto;
+    -webkit-flex: 1 1 auto;
+    -ms-flex: 1 1 auto;
+    flex: 1 1 auto;
+    background-color: #e9e9ed;
+    transition: all 0.2s ease-in-out;
+  }
+  button:first-child {
+    -webkit-border-radius: 10px 0 0 10px;
+    -moz-border-radius: 10px 0 0 10px;
+    border-radius: 10px 0 0 10px;
+  }
+  button:last-child {
+    -webkit-border-radius: 0 10px 10px 0;
+    -moz-border-radius: 0 10px 10px 0;
+    border-radius: 0 10px 10px 0;
+  }
+  button:hover {
+    background-color: #d0d0d7;
+  }
 </style>
