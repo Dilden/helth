f1a3d9cc7eff90fa250a123759835e317a64e33b
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2023-10-03 15:30:44 -0500

    Updated a11y locators for test

--- src/lib/counts/counter.test.js
+++ src/lib/counts/counter.test.js
@@ -4,27 +4,29 @@ import Counter from './Counter.svelte';
 
 describe('counter component', () => {
   it('renders a counter component with matching props', () => {
+    const title = 'counter test';
     render(Counter, {
       count: 10,
-      title: 'counter test',
+      title: title,
       interval: 4,
       max: 150,
       diffString: '100 calories remaining'
     });
-    expect(screen.queryByText('counter test')).toBeVisible();
-    expect(screen.queryByLabelText('Count value')).toHaveValue(10)
+    expect(screen.queryByText(title)).toBeVisible();
+    expect(screen.queryByLabelText(`${title}`)).toHaveValue(10)
     expect(screen.queryByRole('button', { name: '-4'})).toBeVisible()
     expect(screen.queryByRole('button', { name: '+4'})).toBeVisible()
-    expect(screen.queryByLabelText('interval', { type: 'range' })).toHaveValue('4')
+    expect(screen.queryByLabelText(`Adjust interval for ${title}`, { type: 'range' })).toHaveValue('4')
   });
 
   it('changes the count value when a button is clicked', async () => {
+    const title = 'test';
     render(Counter, {
       count: 5,
-      title: 'test',
+      title: title,
       interval: 2
     });
     await screen.queryByRole('button', { name: '-2'}).click();
-    expect(screen.queryByLabelText('Count value')).toHaveValue(3);
+    expect(screen.queryByLabelText(`${title}`)).toHaveValue(3);
   })
 });
