d5a3f27bfd717efc94b5cad6203008b4ff6d78e7
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2023-10-04 22:01:53 -0500

    Removed unused import

--- src/lib/inventory/list.test.js
+++ src/lib/inventory/list.test.js
@@ -1,6 +1,6 @@
 import "fake-indexeddb/auto";
 import { describe, it, expect } from 'vitest';
-import { render, screen, waitFor } from '@testing-library/svelte';
+import { render, screen } from '@testing-library/svelte';
 import List from './List.svelte';
 import { inventory } from '../../stores/stores.js';
 
