c812d78a892ac6d4ee1cffb8ee4d22152f3e4a6c
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2023-10-09 13:04:12 -0500

    Starting recipe logic, tests still not able to add data to fake-indexeddb

---
+++ src/lib/recipes/Recipes.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { recipes } from '$stores/stores.js';
+</script>
+
+<h3>Recipes</h3>
+<ul>
+  <!-- {#await recipes.init()} -->
+      <!-- waiting... -->
+  <!-- {:then} -->
+    before if
+    {#if $recipes.length}
+      has length
+      {#each $recipes as recipe}
+        <li>{recipe.name}</li>
+      {/each}
+    {/if}
+  <!-- {/await} -->
+</ul>
