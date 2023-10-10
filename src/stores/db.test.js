fb1f8a70ce30863d050623e5873920dee05893ef
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2023-10-10 16:18:05 -0500

    Some initial tests that can be used on the DB interfaces

---
+++ src/stores/db.test.js
@@ -0,0 +1,24 @@
+import 'fake-indexeddb/auto';
+import { describe, it, expect, beforeAll } from 'vitest';
+import { dbopen, getListItems, addToList } from '$stores/db.js';
+import { updateItemInList } from './db';
+
+beforeAll(async () => await dbopen);
+
+describe('list tables', () => {
+  it('adds an item to a list', async () => {
+    await addToList('recipes', {name: 'test', description: 'desc'});
+    expect(await getListItems('recipes')).toContainEqual(
+      expect.objectContaining({name: 'test', description: 'desc'})
+    );
+  })
+
+  it('can update an item in the list', async () => {
+    await addToList('recipes', {id: 2, name: 'number 2', description: 'another thing'});
+    await updateItemInList('recipes', 2, {name: 'numero dos'});
+    expect(await getListItems('recipes')).toContainEqual(
+      expect.objectContaining({name: 'numero dos', description: 'another thing'})
+    );
+  })
+
+})
