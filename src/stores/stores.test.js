c86a5abdc16ba0c22b0b46bd80ac5a8f978c17db
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2023-10-11 14:55:24 -0500

    Testing createListStore stores based on recipe

--- src/stores/stores.test.js
+++ src/stores/stores.test.js
@@ -1,19 +1,22 @@
 import 'fake-indexeddb/auto';
-import { describe, it, expect, beforeAll } from 'vitest';
+import { IDBFactory } from 'fake-indexeddb';
+import { describe, it, expect, beforeEach } from 'vitest';
 import { recipes } from '$stores/stores.js';
-import { dbopen } from '$stores/db.js';
+import { getListItems } from '$stores/db.js';
 
-describe('recipes stores', () => {
-  beforeAll(async () => {
-    await dbopen;
-  })
+beforeAll(async () => {
+  indexedDB = new IDBFactory();
+  await recipes.init();
+});
 
+describe.sequential('recipes stores', () => {
   it('can set a value', async () => {
     await recipes.set({
       name: 'first recipe',
       description: 'its just a test, bro'
     });
-    recipes.subscribe((value) => {
+
+    const unsub = recipes.subscribe((value) => {
       expect(value).toContainEqual(
         expect.objectContaining({
           name: 'first recipe',
@@ -21,5 +24,36 @@ describe('recipes stores', () => {
         })
       );
     })
+    unsub();
+  });
+
+  it('can update a value', async () => {
+    const recipesList = await getListItems('recipes');
+
+    await recipes.set({
+      id: recipesList[0].id,
+      name: 'updated',
+      description: recipesList[0].description
+    })
+
+    const unsub = await recipes.subscribe((value) => {
+      expect(value).toContainEqual(
+        expect.objectContaining({
+          name: 'updated',
+          description: 'its just a test, bro'
+        })
+      );
+    })
+    unsub();
+  });
+
+  it('can delete an item', async () => {
+    const recipesList = await getListItems('recipes');
+    await recipes.delete(recipesList[0].id);
+
+    const unsub = await recipes.subscribe((value) => {
+      expect(value).toStrictEqual([]);
+    })
+    unsub();
   })
 })
