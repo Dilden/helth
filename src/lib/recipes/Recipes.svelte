fb116889ee3909b857bcf7244cf26f469e0a8714
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2023-10-12 10:49:01 -0500

    some tests for recipes

--- src/lib/recipes/Recipes.svelte
+++ src/lib/recipes/Recipes.svelte
@@ -1,15 +1,21 @@
 <script>
   import { recipes } from '$stores/stores.js';
+  import { getItemByIdFromTable } from '$stores/db.js';
 </script>
 
 <h3>Recipes</h3>
 <ul>
-  {#await recipes.init()}
-      waiting...
-  {:then}
+  {#await recipes.init() then }
     {#if $recipes.length}
       {#each $recipes as recipe}
         <li>{recipe.name}</li>
+        {#each recipe.items as item}
+          {#await getItemByIdFromTable('inventory', item.id) then inventoryItem}
+            <p>
+              {inventoryItem.name}
+            </p>
+          {/await}
+        {/each}
       {/each}
     {/if}
   {/await}
--- src/lib/recipes/recipe.test.js
+++ src/lib/recipes/recipe.test.js
@@ -2,22 +2,60 @@ import 'fake-indexeddb/auto';
 import { IDBFactory } from 'fake-indexeddb';
 import { render, screen } from '@testing-library/svelte';
 import { describe, it, expect, vi } from 'vitest';
-import { recipes } from '$stores/stores.js';
+import { recipes, inventory } from '$stores/stores.js';
 import Recipes from './Recipes.svelte';
 
 beforeAll(async () => {
   indexedDB = new IDBFactory();
   await recipes.init();
+  await inventory.set({ 
+    name: 'Coca-Cola',
+    description: 'a carbonated beverage that will rot your teeth',
+    nutrients: {
+      calories: {
+        name: 'Calories',
+        quantity: '200',
+        unit: 'kcal'
+      },
+      added_sugars: {
+        name: 'Added Sugars',
+        quantity: '300',
+        unit: 'g'
+      }
+    }
+  });
+  await inventory.set({ 
+    name: 'Water',
+    description: 'hydrohomie 4 life',
+    nutrients: {
+      calories: {
+        name: 'Calories',
+        quantity: '0',
+        unit: 'kcal'
+      }
+    }
+  });
+  await inventory.set({ 
+    name: 'Syrup',
+    description: 'sticky',
+    nutrients: {
+      calories: {
+        name: 'Calories',
+        quantity: '400',
+        unit: 'kcal'
+      }
+    }
+  });
 });
 
-describe('recipe list', () => {
+describe.sequential('recipe list', () => {
 
   it('has a heading', () => {
     render(Recipes);
     expect(screen.queryByRole('heading', {name: 'Recipes'})).toBeVisible();
   });
 
-  it('renders a list of items from the recipe store', async () => {
+  it('shows recipe titles from the recipe store', async () => {
     await recipes.set({
       name: 'Brownies',
       description: 'Children will eat this',
@@ -29,5 +67,11 @@ describe('recipe list', () => {
     render(Recipes);
 
     expect(await screen.findByText('Brownies')).toBeVisible();
+  })
+
+  it('gets names of inventory items in recipe', async () => {
+    render(Recipes);
+    expect(await screen.findByText('Coca-Cola')).toBeVisible();
+    expect(await screen.findByText('Syrup')).toBeVisible();
   })
 })
