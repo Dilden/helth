81cdafce2205d25b04b44cd9c6956219d06e43e3
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2023-10-17 14:10:10 -0500

    Extracted logic out of formValues to another function since formData for recipes also needs to be formatted.

--- src/utils/formValues.js
+++ src/utils/formValues.js
@@ -3,9 +3,36 @@ import { list } from '$utils/nutrients.js';
 export const formValues = (formCollection) => {
   const formFields = formCollection.elements;
 
-  const textValues = Object.values(formFields)
-  .filter((item) => (item.type === 'text' || item.type === 'hidden' ))
+  const values = Object.values(formFields)
+  .filter((item) => (item.type !== 'submit'))
   .filter((item) => (item.value))
+
+  return values;
+}
+
+export const formatRecipeFormValues = (formData) => {
+  return formValues( formData )
+  .reduce((accum, current) => {
+      console.log(current)
+    // if( current.name!=='name' && current.name!=='description' && current.name!=='id' ) {
+    if( current.type==='checkbox' && current.checked ) {
+      accum['items'] = accum['items'] || [];
+
+      accum['items'].push({id: Number( current.value )});
+    }
+    else {
+      // if(current.name === 'id') {
+      //   accum[current.name] = Number(current.value);
+      // } else {
+        accum[current.name] = current.value
+      // }
+    }
+    return accum;
+  }, {});
+}
+
+export const formatInventoryFormValues = (formData) => {
+  return formValues( formData )
   .reduce((accum, current) => {
     if( current.name!=='name' && current.name!=='description' && current.name!=='barcode' && current.name!=='id' ) {
       // if nutrients doesn't exist, create it
@@ -26,6 +53,4 @@ export const formValues = (formCollection) => {
     }
     return accum;
   }, {});
-
-  return textValues;
 }
--- src/utils/formvalues.test.js
+++ src/utils/formvalues.test.js
@@ -1,5 +1,9 @@
 import { describe, it, expect } from 'vitest';
-import { formValues } from '$utils/formValues.js';
+import { 
+  formValues, 
+  formatInventoryFormValues,
+  formatRecipeFormValues
+} from '$utils/formValues.js';
 
 const sample = {
   elements: {
@@ -27,13 +31,65 @@ const sample = {
       type: 'text',
       value: '10'
     },
-    length: 5
+    5: {
+      name: 'submit',
+      type: 'submit',
+      value: ''
+    },
+    length: 6
+  }
+}
+
+const sampleRecipe = {
+  elements: {
+    0: {
+      name: 'name',
+      type: 'text',
+      value: 'pizza'
+    },
+    1: {
+      name: 'description',
+      type: 'text',
+      value: 'friday night is pizza night'
+    },
+    2: {
+      name: 'cheese',
+      type: 'checkbox',
+      value: '34'
+    },
+    3: {
+      name: 'sauce',
+      type: 'checkbox',
+      value: '2'
+    },
+    4: {
+      name: 'crust',
+      type: 'checkbox',
+      value: '106'
+    },
+    5: {
+      name: 'submit',
+      type: 'submit',
+      value: ''
+    },
+    length: 6
   }
 }
 
 describe('form values', () => {
-  it('returns an object of key:value pairs + nested nutrients object from the form values', async () => {
+  it('returns form input values except submit', () => {
     const results = formValues(sample);
+    expect(results).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({name: 'name'}),
+        expect.objectContaining({name: 'description'}),
+        expect.objectContaining({name: 'added_sugars'}),
+        expect.objectContaining({name: 'calories'}),
+      ])
+    );
+  })
+  it('returns an object of key:value pairs + nested nutrients object from the form values', async () => {
+    const results = formatInventoryFormValues(sample);
     expect(results).toEqual({ 
       'name': 'New inventory item',
       'description': 'Item description',
@@ -51,4 +107,17 @@ describe('form values', () => {
       }
     })
   });
+
+  it('formats values from recipes form', () => {
+    const results = formatRecipeFormValues(sampleRecipe);
+    expect(results).toEqual({
+      name: 'pizza',
+      description: 'friday night is pizza night',
+      items: [
+        {id: 34},
+        {id: 2},
+        {id: 106}
+      ]
+    })
+  })
 })
