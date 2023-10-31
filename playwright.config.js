d440e265e74b344222b5e794bd9367d4c5713dce
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2023-10-31 11:29:38 -0500

    Save traces of tests so reports can be viewed after failed tests in Playwright.

--- playwright.config.js
+++ playwright.config.js
@@ -4,7 +4,11 @@ const config = {
     command: 'npm run build && npm run preview',
     port: 4173
   },
-  testDir: 'tests'
+  testDir: 'tests',
+  use: {
+    trace: 'on-first-retry'
+  },
+  retries: process.env.CI ? 2 : 0
 };
 
 export default config;
