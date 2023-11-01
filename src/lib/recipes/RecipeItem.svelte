fd80a45b91f2b7daa3ae4d9f36b25dfe70545ac7
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2023-11-01 11:27:34 -0500

    Refactored to reduce number of props exported, added delete recipe feature

--- src/lib/recipes/RecipeItem.svelte
+++ src/lib/recipes/RecipeItem.svelte
@@ -1,29 +1,42 @@
 <script>
   import { nutrientSumsFromList } from '$utils/item.js';
   import { list } from '$utils/nutrients.js';
+  import { confirmDialog, successToast, errorToast} from '$utils/toast.js';
+  import { recipes } from '$stores/stores.js';
 
-  export let name = '';
-  export let description = '';
-  export let items = [];
+  export let recipe = {};
+
+  const confirmDelete = () => {
+    confirmDialog('Are you sure you want to delete this item?', deleteRecipe, () => false);
+  }
+
+  const deleteRecipe = () => {
+    recipes.delete(recipe.id)
+    .then(() => successToast('Removed recipe!'))
+    .catch(() => errorToast('Error deleting recipe!'));
+  }
+
 </script>
-<h4>{name}</h4>
-<p>{description}</p>
+
+<h4>{recipe.name}</h4>
+<p>{recipe.description}</p>
 <div>
   <ul class='items'>
-    {#each items as item}
+    {#each recipe.items as item}
       <li>
         {item.name}
       </li>
     {/each}
   </ul>
   <ul class='nutrients'>
-    {#each Object.entries(nutrientSumsFromList(items)) as nutrient}
+    {#each Object.entries(nutrientSumsFromList(recipe.items)) as nutrient}
       {#if nutrient[1]}
         <li>{list[nutrient[0]].name + ': ' + nutrient[1] + list[nutrient[0]].unit}</li>
       {/if}
     {/each}
   </ul>
 </div>
+<button on:click={confirmDelete} title="Delete Recipe">🗑️</button> <!-- remove from db -->
 
 <style>
   ul {
