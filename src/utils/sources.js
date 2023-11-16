49637fe7726d1a5ed97879ddb526016958a82b34
dylan <dylan.hildenbrand@gmail.com>
2023-11-16 12:51:36 -0600

    Get and format data from OpenFoodFacts API

--- src/utils/sources.js
+++ src/utils/sources.js
@@ -4,17 +4,19 @@ import { error } from '@sveltejs/kit';
 
 const openFoodFacts = (code) => {
   // // staging data
-  return `https://world.openfoodfacts.net/api/v2/product/${code}.json?fields=product_name,nutriments,ingredients_text`
-  // // production data
-  // return `https://world.openfoodfacts.org/api/v2/product/${code}.json`
+  // return `https://world.openfoodfacts.net/api/v2/product/${code}.json?fields=product_name,nutriments,ingredients_text,serving_size,serving_quantity`
+  // production data
+  return `https://world.openfoodfacts.org/api/v2/product/${code}.json?fields=product_name,nutriments,ingredients_text,serving_size,serving_quantity`
 }
 
 const formatOpenFood = (data) => {
   const formatted = {};
-  formatted.name = data.product.product_name;
+  formatted.name = data?.product?.product_name_en || data?.product?.product_name;
   formatted.description = data.product.ingredients_text;
   formatted.barcode = data.code;
 
+  // nutrients require bulk of the effort to format
+  // since Open Food Facts API data is often incomplete
   formatted.nutrients = Object.entries( list ).reduce((accum, [key, value]) => {
 
     let nutrimentKey = '';
@@ -33,23 +35,44 @@ const formatOpenFood = (data) => {
         nutrimentKey = 'carbohydrate';
         break;
       case 'total-fat':
-        accum[key].quantity = 'fat';
+        nutrimentKey = 'fat';
         break;
       default:
         nutrimentKey = key;
         break;
     }
 
-    if(data?.product?.nutriments[nutrimentKey]) {
+    if(data?.product?.nutriments[nutrimentKey] && data.product.nutriments[nutrimentKey + '_unit'] !== ('dv' || '% vol')) {
       accum[key] = accum[key] || {};
       accum[key].name = value.name;
       accum[key].unit = value.unit;
-      if(data.product.nutriments[nutrimentKey + '_unit'] === ( 'mg' || 'ml' )) {
-        accum[key].quantity = data.product.nutriments[nutrimentKey] * 1000;
+
+      let modifier = 1;
+      switch(data.product.nutriments[nutrimentKey + '_unit']) {
+        case 'mg':
+        case 'ml':
+          modifier = 1000;
+          break;
+        case 'μg':
+          modifier = 10000;
+          break;
+        case 'cl':
+          modifier = 100;
+          break
+        default:
+          modifier = 1;
+          break;
       }
-      else {
-        accum[key].quantity = data.product.nutriments[nutrimentKey];
-      }
+
+      // Data returned from the API isn't always complete.
+      // One of the downsides to crowdsourcing.
+      // This is a best effort to get all values according to available data.
+      const amount =  (data?.product?.nutriments[nutrimentKey + '_100g'] * data?.product?.serving_quantity * modifier) / 100
+        || data?.product?.nutriments[nutrimentKey + '_value_computed']
+        || data?.product?.nutriments[nutrimentKey + '_serving'] * modifier 
+        || data.product.nutriments[nutrimentKey] * modifier;
+
+      accum[key].quantity = Math.round(amount);
     }
     return accum;
   }, {});
