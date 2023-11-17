f787101b0884a8d67ee32e03af99cca75af35eaa
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2023-11-17 15:25:56 -0600

    updated tests to look for icon emoji before title text

--- tests/goals.test.js
+++ tests/goals.test.js
@@ -2,5 +2,5 @@ import { expect, test } from '@playwright/test';
 
 test('goal page has expected h2', async ({ page }) => {
   await page.goto('/goals/');
-  expect(await page.textContent('h2')).toBe('goals 🥇');
+  expect(await page.textContent('h2')).toBe('🥇 goals');
 });
