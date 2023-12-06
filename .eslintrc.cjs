3e4cffdf29e2069c20f7f397089979f5da84af36
dylan <dylan.hildenbrand@gmail.com>
2023-12-06 15:08:12 -0600

    eslint config copied from skeleton project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,30 @@
 module.exports = {
-    root: true,
-    extends: ['eslint:recommended', 'prettier'],
-    plugins: ['svelte3'],
-    overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-    parserOptions: {
-        sourceType: 'module',
-        ecmaVersion: 2020
-    },
-    env: {
-        browser: true,
-        es2017: true,
-        node: true
-    }
+  root: true,
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended',
+		'prettier'
+	],
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint'],
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 2020,
+		extraFileExtensions: ['.svelte']
+	},
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
+		}
+	],
+  env: {
+      browser: true,
+      es2017: true,
+      node: true
+  }
 };
