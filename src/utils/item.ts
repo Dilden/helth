1817031c7ff5b96fbf81106947430753723727fd
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2023-12-14 10:11:13 -0600

    Ensure only one of each nutrient is returned in nutrientSumsFromList before displaying in component

--- src/utils/item.ts
+++ src/utils/item.ts
@@ -3,10 +3,11 @@ export const nutrientSumsFromList = (itemList: InventoryItem[] ) => {
   const allNutrients = itemList.map(( item ) => item.nutrients).flat();
 
   const sums = allNutrients.reduce((accum, current: Nutrient) => {
-    const found = accum.find((item: Nutrient) => item.key === current.key);
 
-    if(found && found.quantity && current.quantity) {
-      found.quantity += current.quantity;
+    const foundIndex = accum.findIndex(({key}) => key === current.key);
+
+    if(accum && accum[foundIndex]) {
+      accum[foundIndex].quantity = Number( accum[foundIndex].quantity ) + Number( current.quantity );
     }
     else {
       accum.push(current);
