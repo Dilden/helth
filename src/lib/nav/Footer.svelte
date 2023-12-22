c092f7ff3b6e94a2e11a95be4da7d2f10204354d
dylan <dylan.hildenbrand@gmail.com>
2023-12-22 16:56:48 -0600

    new logo?

--- src/lib/nav/Footer.svelte
+++ src/lib/nav/Footer.svelte
@@ -1,14 +1,9 @@
-<footer>
-  <p>
+<script>
+  import meme from './meme.png';
+</script>
+<footer class="text-center">
+  <img src="{meme}" class="fixed left-0 bottom-0" alt="Meme man wearing a lab coat and stehoscope" width="200" height="auto" />
+  <p class="p-0 pt-8">
     made w/💕 by <a href="https://www.closingtags.com">closingtags</a>
   </p>
 </footer>
-
-<style>
-  footer {
-    text-align: center;
-  }
-  p {
-    padding: 30px 0 0;
-  }
-</style>
--- src/lib/nav/Helth.svelte
+++ src/lib/nav/Helth.svelte
@@ -1,35 +1,24 @@
-<a href="/">
-  <img src="/helth.jpg" alt="Meme man wearing a lab coat and stehoscope" width="200" height="auto" />
-  <h1>helth app</h1>
+<script>
+  import logoUrl from './logo.svg';
+</script>
+<a href="/" class="hover:no-underline">
+  <img src="{logoUrl}" alt="Meme man wearing a lab coat and stehoscope" width="200" height="auto" class="w-auto max-w-xs h-full m-0 p-0 inline-block align-middle"/>
+  <h1 class="align-middle inline-block text-[--fore-color]">helth.app</h1>
 </a>
 
 <style>
+  @media screen and (max-width: 925px) {
+    h1 {
+      font-size: calc( 1rem * var(--heading-ratio) * var(--heading-ratio) * var(--heading-ratio) );
+    }
+  }
+  @media screen and (max-width: 500px) {
+    h1 {
+      font-size: calc( 1rem * var(--heading-ratio) );
+    }
     img {
         width: auto;
-        max-width: 150px;
-        height: 100%;
-        margin: 0;
-        padding: 0;
-        display: inline-block;
-        vertical-align: middle;
+        max-width: 120px;
     }
-    h1 {
-        display: inline-block;
-        color: var(--fore-color);
-        vertical-align: middle;
-    }
-    @media screen and (max-width: 925px) {
-      h1 {
-        font-size: calc( 1rem * var(--heading-ratio) * var(--heading-ratio) * var(--heading-ratio) );
-      }
-    }
-    @media screen and (max-width: 500px) {
-      h1 {
-        font-size: calc( 1rem * var(--heading-ratio) );
-      }
-      img {
-          width: auto;
-          max-width: 120px;
-      }
-    }
+  }
 </style>
