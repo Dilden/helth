560c7a1649bc8b06fb8360033b81da9a0ffb4273
dylan <dylan.hildenbrand@gmail.com>
2024-01-03 15:35:53 -0600

    Logo CSS moved to Tailwind, updated a test to reflect the app name title at the top of the page.

--- src/lib/nav/Helth.svelte
+++ src/lib/nav/Helth.svelte
@@ -1,24 +1,13 @@
 <script>
-  import logoUrl from './logo.svg';
+	import logoUrl from './logo.svg';
 </script>
-<a href="/" class="hover:no-underline">
-  <img src="{logoUrl}" alt="Meme man wearing a lab coat and stehoscope" width="200" height="auto" class="w-auto max-w-xs h-full m-0 p-0 inline-block align-middle"/>
-  <h1 class="align-middle inline-block text-[--fore-color]">helth.app</h1>
-</a>
 
-<style>
-  @media screen and (max-width: 925px) {
-    h1 {
-      font-size: calc( 1rem * var(--heading-ratio) * var(--heading-ratio) * var(--heading-ratio) );
-    }
-  }
-  @media screen and (max-width: 500px) {
-    h1 {
-      font-size: calc( 1rem * var(--heading-ratio) );
-    }
-    img {
-        width: auto;
-        max-width: 120px;
-    }
-  }
-</style>
+<a href="/" class="hover:no-underline">
+  <span class="w-20 lg:w-32 m-0 inline-block h-auto p-2 align-middle"><img
+    src={logoUrl}
+    alt="A chicken drumstick with a bite take out of the meatiest portion postitioned inside the viewport of a barcode scanner."
+    width="auto"
+    height="auto"
+    class="max-w-full max-h-full"
+  /></span><h1 class="inline-block align-middle text-3xl text-[--fore-color] lg:text-5xl">helth.app</h1>
+</a>
--- tests/index.test.js
+++ tests/index.test.js
@@ -2,5 +2,5 @@ import { expect, test } from '@playwright/test';
 
 test('index page has expected h1', async ({ page }) => {
   await page.goto('/');
-  expect(await page.textContent('h1')).toBe('helth app');
+  expect(await page.textContent('h1')).toBe('helth.app');
 });
