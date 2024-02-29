d0aba7602091686ff475e091abbe5a435ec9fc92
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2024-02-29 14:25:50 -0600

    Formatting and bringing tests inline with changes to related code

--- tests/adddialog.test.js
+++ tests/adddialog.test.js
@@ -13,12 +13,12 @@ test.describe('add items dialog', () => {
     });
 
     test('add items to inventory form is hidden by default', async ({ page }) => {
-      await expect(page.getByLabel('Name', {exact: true})).not.toBeVisible();
-      await expect(page.getByLabel('Calories', {exact: true})).not.toBeVisible();
+      await expect(page.getByLabel('Name', { exact: true })).not.toBeVisible();
+      await expect(page.getByLabel('Calories', { exact: true })).not.toBeVisible();
     });
 
     test('clicking Add Item shows form inputs', async ({ page }) => {
-      await page.getByRole('button', { name: 'Add Item'}).click();
+      await page.getByRole('button', { name: 'Add Item' }).click();
       await expect(page.getByLabel('Name')).toBeVisible();
       await expect(page.getByLabel('Added Sugars')).toBeVisible();
       await expect(page.getByLabel('Iron')).toBeVisible();
@@ -26,25 +26,25 @@ test.describe('add items dialog', () => {
 
     // run the next few tests in sequence
     test.describe('item management', () => {
-      test.describe.configure({ mode: 'serial'});
-      
+      test.describe.configure({ mode: 'serial' });
+
       let page;
-      test.beforeAll(async ({browser}) => {
+      test.beforeAll(async ({ browser }) => {
         page = await browser.newPage();
         await page.goto('/');
-        await page.getByRole('status').getByRole('button', {name: 'Yes'}).click();
+        await page.getByRole('status').getByRole('button', { name: 'Yes' }).click();
         await page.getByRole('button', { name: 'Open Add Dialog' }).click();
       })
 
       test('manually adding an item to the inventory shows it on the page and clears inputs', async () => {
-        await page.getByRole('button', { name: 'Add Item'}).click(); 
+        await page.getByRole('button', { name: 'Add Item' }).click();
         await page.getByLabel('Name').fill('Sample Item X');
         await page.getByLabel('Barcode').fill('123456789012');
         await page.getByLabel('Description').fill('This item represents a sample item');
-        await page.getByLabel('Calories', {exact: true}).fill('100');
-        await page.getByLabel('Sodium', {exact: true}).fill('20');
-        await page.getByLabel('Protein', {exact: true}).fill('20');
-        await page.getByRole('button', {name: 'Save'}).click();
+        await page.getByLabel('Calories', { exact: true }).fill('100');
+        await page.getByLabel('Sodium', { exact: true }).fill('20');
+        await page.getByLabel('Protein', { exact: true }).fill('20');
+        await page.getByRole('button', { name: 'Save' }).click();
 
         await expect(page.getByText('Sample Item X')).toBeVisible();
         await expect(page.getByText('Sodium: 20mg')).toBeVisible();
@@ -53,18 +53,18 @@ test.describe('add items dialog', () => {
       test('add item in inventory to daily total', async () => {
         await page.getByTitle('Add to Daily Total').click();
         await page.getByTitle('Add to Daily Total').click();
-        await page.getByRole('button', {name: 'Close Add Dialog'}).click();
+        await page.getByRole('button', { name: 'Close Add Dialog' }).click();
 
-        await expect( page.getByLabel('⚡ calories', { exact: true }) ).toHaveValue('200');
-        await expect( page.getByLabel('🧂 sodium (mg)', { exact: true }) ).toHaveValue('40');
-        
+        await expect(page.getByLabel('⚡ calories', { exact: true })).toHaveValue('200');
+        await expect(page.getByLabel('🧂 sodium (mg)', { exact: true })).toHaveValue('40');
+
       })
       test('edit item in inventory', async () => {
-        await page.getByRole('button', {name: 'Open Add Dialog'}).click();
+        await page.getByRole('button', { name: 'Open Add Dialog' }).click();
         await page.getByRole('listitem')
-        .filter({hasText: 'Sample Item X'})
-        .getByRole('button', {name: '✏️'})
-        .click();
+          .filter({ hasText: 'Sample Item X' })
+          .getByRole('button', { name: '✏️' })
+          .click();
 
         await page.getByLabel('inventory-list').locator('#name').fill('New Sample Item');
         await page.getByPlaceholder('kcal').fill('200');
@@ -87,13 +87,13 @@ test.describe('add items dialog', () => {
 
       test('delete item in inventory', async () => {
         await page.getByPlaceholder('Begin typing...').fill('');
-        await page.getByRole('button', {name: '➕'}).click();
+        await page.getByRole('button', { name: '➕' }).click();
         await page.getByRole('listitem')
-        .filter({hasText: 'New Sample Item'})
-        .getByRole('button', {name: '🗑️'})
-        .click();
+          .filter({ hasText: 'New Sample Item' })
+          .getByRole('button', { name: '🗑️' })
+          .click();
 
-        await page.getByRole('button', {name: 'Yes'}).click();
+        await page.getByRole('button', { name: 'Yes' }).click();
 
         await expect(page.getByText('New Sample Item')).not.toBeVisible();
       })
@@ -103,33 +103,33 @@ test.describe('add items dialog', () => {
 
   test.describe('recipes', () => {
 
-    test.describe.configure({ mode: 'serial'});
+    test.describe.configure({ mode: 'serial' });
     let page;
 
-    test.beforeAll(async ({browser}) => {
+    test.beforeAll(async ({ browser }) => {
       page = await browser.newPage();
       await page.goto('/');
       await page.locator('li').filter({ hasText: 'Don\'t lose your data! Make storage persistent now? Yes No' }).getByTitle('Yes').click();
       await page.getByRole('button', { name: 'Open Add Dialog' }).click();
 
-      await page.getByRole('button', { name: 'Add Item'}).click(); 
+      await page.getByRole('button', { name: 'Add Item' }).click();
       await page.getByLabel('Name').fill('Coca-Cola');
       await page.getByLabel('Barcode').fill('111111111111');
       await page.getByLabel('Description').fill('sweet nectar of the gods');
-      await page.getByLabel('Calories', {exact: true}).fill('250');
-      await page.getByLabel('Sodium', {exact: true}).fill('20');
-      await page.getByLabel('Protein', {exact: true}).fill('0');
-      await page.getByRole('button', {name: 'Save'}).click();
+      await page.getByLabel('Calories', { exact: true }).fill('250');
+      await page.getByLabel('Sodium', { exact: true }).fill('20');
+      await page.getByLabel('Protein', { exact: true }).fill('0');
+      await page.getByRole('button', { name: 'Save' }).click();
 
 
-      await page.getByRole('button', { name: 'Add Item'}).click(); 
+      await page.getByRole('button', { name: 'Add Item' }).click();
       await page.getByLabel('Name').fill('Pepsi');
       await page.getByLabel('Barcode').fill('222222222222');
       await page.getByLabel('Description').fill('will strip your insides clean');
-      await page.getByLabel('Calories', {exact: true}).fill('250');
-      await page.getByLabel('Sodium', {exact: true}).fill('20');
-      await page.getByLabel('Protein', {exact: true}).fill('0');
-      await page.getByRole('button', {name: 'Save'}).click();
+      await page.getByLabel('Calories', { exact: true }).fill('250');
+      await page.getByLabel('Sodium', { exact: true }).fill('20');
+      await page.getByLabel('Protein', { exact: true }).fill('0');
+      await page.getByRole('button', { name: 'Save' }).click();
     });
 
     test('saves a recipe to the DB', async () => {
@@ -139,7 +139,7 @@ test.describe('add items dialog', () => {
       await page.getByLabel('Description').fill('horrid concoction');
       await page.getByLabel('Coca-Cola').check();
       await page.getByLabel('Pepsi').check();
-      await page.getByRole('button', {name: 'Save'}).click();
+      await page.getByRole('button', { name: 'Save' }).click();
 
 
       // await expect(page.getByLabel('Name')).toBeEmpty();
@@ -153,35 +153,35 @@ test.describe('add items dialog', () => {
       // await expect(page.getByText('Protein: 0mg')).not.toBeVisible();
     })
 
-    test('can search to filter inventory items in recipe form', async () => {
+    test('can filter inventory items in recipe form', async () => {
       await page.getByRole('button', { name: 'Add Recipe' }).click();
-      await page.getByLabel('Search inventory').fill('Pepsi');
-      
+      await page.getByLabel('Filter inventory').fill('Pepsi');
+
       await expect(page.getByLabel('Pepsi')).toBeVisible();
       await expect(page.getByLabel('Coca-Cola')).not.toBeVisible();
 
-      await page.getByLabel('Search inventory').fill('');
-      await page.getByRole('button', {name: 'Save'}).click();
+      await page.getByLabel('Filter inventory').fill('');
+      await page.getByRole('button', { name: 'Save' }).click();
     })
 
     test('adds a recipe to the current daily total', async () => {
-        await page.locator('li').filter({hasText: 'Soda'}).getByTitle('Add to Daily Total').click();
-        await page.locator('li').filter({hasText: 'Soda'}).getByTitle('Add to Daily Total').click();
-        await page.getByRole('button', {name: 'Close Add Dialog'}).click();
+      await page.locator('li').filter({ hasText: 'Soda' }).getByTitle('Add to Daily Total').click();
+      await page.locator('li').filter({ hasText: 'Soda' }).getByTitle('Add to Daily Total').click();
+      await page.getByRole('button', { name: 'Close Add Dialog' }).click();
 
-        // await expect( page.getByLabel('⚡ calories', { exact: true }) ).toHaveValue('1000');
-        await expect( page.getByLabel('⚡ calories', { exact: true }) ).not.toHaveValue('0');
-        // await expect( page.getByLabel('🧂 sodium (mg)', { exact: true }) ).toHaveValue('80');
-        await expect( page.getByLabel('🧂 sodium (mg)', { exact: true }) ).not.toHaveValue('0');
+      // await expect( page.getByLabel('⚡ calories', { exact: true }) ).toHaveValue('1000');
+      await expect(page.getByLabel('⚡ calories', { exact: true })).not.toHaveValue('0');
+      // await expect( page.getByLabel('🧂 sodium (mg)', { exact: true }) ).toHaveValue('80');
+      await expect(page.getByLabel('🧂 sodium (mg)', { exact: true })).not.toHaveValue('0');
     })
-    
+
     test('edits a recipe', async () => {
-      await page.getByRole('button', {name: 'Open Add Dialog'}).click();
-      await page.getByRole('button', {name: 'Recipes'}).click();
+      await page.getByRole('button', { name: 'Open Add Dialog' }).click();
+      await page.getByRole('button', { name: 'Recipes' }).click();
       await page.getByRole('listitem')
-      .filter({hasText: 'Soda'})
-      .getByRole('button', {name: '✏️'})
-      .click();
+        .filter({ hasText: 'Soda' })
+        .getByRole('button', { name: '✏️' })
+        .click();
 
       // doing this on the wrong form
       await page.locator('li').getByLabel('Recipe Name').fill('Soda 2');
@@ -193,12 +193,12 @@ test.describe('add items dialog', () => {
       await expect(page.locator('li').getByText('better w/o that garbage')).toBeVisible();
       await expect(page.locator('li').getByText('Pepsi')).not.toBeVisible();
     })
-    
+
     test('can delete a recipe', async () => {
       await page.getByRole('listitem')
-      .filter({hasText: 'Soda'})
-      .getByRole('button', {name: '🗑️'})
-      .click();
+        .filter({ hasText: 'Soda' })
+        .getByRole('button', { name: '🗑️' })
+        .click();
 
       await page.locator('li').filter({ hasText: 'Are you sure you want to delete this item? Yes No' }).getByTitle('Yes').click();
 
