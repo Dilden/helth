b908edd3448a032b7455fa8c98a37f9427cd6206
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2024-03-20 11:23:24 -0500

    Only show up to 2 decimal points for remaining difference of daily total + goal/limit

--- src/lib/counts/Protein.svelte
+++ src/lib/counts/Protein.svelte
@@ -1,45 +1,49 @@
 <script>
-  import { onMount, afterUpdate } from 'svelte';
-  import { today, settings, goals } from '$stores/stores';
-  import Counter from '$lib/counts/Counter.svelte';
-  import Spinner from '$lib/Spinner.svelte';
+	import { onMount, afterUpdate } from 'svelte';
+	import { today, settings, goals } from '$stores/stores';
+	import { toTwoDecimals } from '$utils/numbers';
+	import Counter from '$lib/counts/Counter.svelte';
+	import Spinner from '$lib/Spinner.svelte';
 
-  let title = '🍗 protein (g)';
+	let title = '🍗 protein (g)';
 
-  $: diff = 0;
-  $: diffString = 'loading...';
+	$: diff = 0;
+	$: diffString = 'loading...';
 
-  const diffUpdate = () => {
-    if('protein' in $goals) {
-      diff = $goals.protein.value - $today.protein;
-      diffString = (diff >= 0 ) ? `${diff} remaining` : `${-diff} over goal!`;
-    }
-    else {
-      diffString = `<a data-sveltekit-reload href='/'>Oops! Click to refresh</a>`;
-    }
-  }
+	const diffUpdate = () => {
+		if ('protein' in $goals) {
+			diff = toTwoDecimals($goals.protein.value - $today.protein);
+			diffString = diff >= 0 ? `${diff} remaining` : `${-diff} over goal!`;
+		} else {
+			diffString = `<a data-sveltekit-reload href='/'>Oops! Click to refresh</a>`;
+		}
+	};
 
-  onMount(() => {
-    goals.init()
-    .then(() => {
-      diffUpdate();
-    })
-  })
-  afterUpdate(() => {
-    diffUpdate();
-  })
+	onMount(() => {
+		goals.init().then(() => {
+			diffUpdate();
+		});
+	});
+	afterUpdate(() => {
+		diffUpdate();
+	});
 </script>
 
 {#await today.init()}
-  <Spinner />
+	<Spinner />
 {:then}
-  {#await settings.init()}
-    <Spinner />
-  {:then}
-    <Counter {title} {diffString} bind:interval={$settings.proteinInterval.value} bind:count={$today.protein}/>
-  {:catch error}
-    <p>Settings error: {error}</p>
-  {/await}
+	{#await settings.init()}
+		<Spinner />
+	{:then}
+		<Counter
+			{title}
+			{diffString}
+			bind:interval={$settings.proteinInterval.value}
+			bind:count={$today.protein}
+		/>
+	{:catch error}
+		<p>Settings error: {error}</p>
+	{/await}
 {:catch error}
-  <p>Error creating 'today': {error}</p>
+	<p>Error creating 'today': {error}</p>
 {/await}
