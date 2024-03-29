1fd54b7ea052b68c86a1b83080118c713ed8d47b
dylan <dylan.hildenbrand@gmail.com>
2024-03-29 16:06:06 -0500

    Removed goals page, moving it to settings. Need to set goals and limits for each nutrient or allow neither or either to be set.

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,10 +1,19 @@
 <script>
+	import { settings, limits, goals } from '$stores/stores';
 	import ExportData from '$lib/data/ExportData.svelte';
 	import ImportData from '$lib/data/ImportData.svelte';
+	import Spinner from '$lib/Spinner.svelte';
 </script>
 
 <div class="p-7 text-center">
 	<h2>⚙️ settings</h2>
+	{#await Promise.all([settings.init(), limits.init(), goals.init()])}
+		<Spinner />
+	{:then}
+		<h3>🏅goals and limits</h3>
+		sgolkdfj
+	{/await}
+	<h3>💾 data</h3>
 	<div class="mx-auto my-6"><ExportData /></div>
 	<div><ImportData /></div>
 </div>
