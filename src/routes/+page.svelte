46781cfc952796d1f4e08a59deff8a8d70b1bcd8
dylan <dylan.hildenbrand@gmail.com>
2024-05-15 16:46:59 -0500

    Tired of limitations of svelte-dnd-action and how it works. I want to try using the browser APIs that exist for drag and drop instead.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,13 @@
 	import { today, settings, limits, goals } from '$stores/stores';
 	import { blur } from 'svelte/transition';
 	import { flip } from 'svelte/animate';
-	import { dndzone, overrideItemIdKeyNameBeforeInitialisingDndZones } from 'svelte-dnd-action';
+	// import { dndzone } from 'svelte-dnd-action';
 	import { onMount, afterUpdate } from 'svelte';
 	import Counter from '$lib/counts/Counter.svelte';
 	import Date from '$lib/Date.svelte';
 	import Add from '$lib/Add.svelte';
 	import Spinner from '$lib/Spinner.svelte';
 
-	overrideItemIdKeyNameBeforeInitialisingDndZones('key');
 	$: enabled = list;
 	onMount(async () => {
 		await settings.init();
@@ -30,10 +29,10 @@
 						return undefined;
 					}
 				})
-				.filter((val) => val !== undefined);
-			// // sort by numerical property
-			// .sort((a, b) => a.position - b.position);
-			// // sort alphabetically
+				.filter((val) => val !== undefined)
+				.sort((a, b) => a.position - b.position);
+			// sort by numerical property
+			// sort alphabetically
 			// .sort((a, b) => {
 			// 	if (a.key > b.key) {
 			// 		return -1;
@@ -46,42 +45,60 @@
 		}
 	};
 
-	const handleMove = (e) => {
-		console.log(e);
-		enabled = e.detail.items;
+	// const handleMove = (e) => {
+	// 	console.log(e);
+	// 	enabled = e.detail.items;
+	// };
+	const dragStartHandler = (event) => {
+		console.log(event);
+		event.dataTransfer.setData('text/html', event.target.outerHTML);
+	};
+	const dragOverHandler = (event) => {
+		event.preventDefault();
+		event.dataTransfer.dropEffect = 'move';
+	};
+	const dropHandler = (event) => {
+		event.preventDefault();
+		const data = event.dataTransfer.getData('text/html');
+		console.log(data);
+		event.target.innerHTML += data;
 	};
 </script>
 
 <h2 class="text-center">🗒 track</h2>
 <Date />
 
-<div
+<p
+	on:dragover={dragOverHandler}
+	on:drop={dropHandler}
 	class="flex-start flex w-full flex-row flex-wrap justify-center gap-4 gap-y-7 md:justify-start md:gap-y-3"
 >
+	<!-- use:dndzone={{ items: enabled, flipDurationMs: 300 }} -->
+	<!-- on:consider={handleMove} -->
+	<!-- on:finalize={handleMove} -->
 	{#await Promise.all([settings.init(), today.init(), limits.init(), goals.init()])}
 		<Spinner />
 	{:then}
 		{#if enabled.length !== 0}
-			<div
-				class="m-auto flex-[2_1_auto] sm:max-w-full md:max-w-[65%] lg:max-w-[30%]"
-				use:dndzone={{ items: enabled, flipDurationMs: 300 }}
-				on:consider={handleMove}
-				on:finalize={handleMove}
-			>
-				{#each enabled as nutrient (nutrient.key)}
-					<div transition:blur animate:flip={{ duration: 900 }}>
-						<Counter
-							item={nutrient}
-							bind:count={$today[nutrient.key]}
-							bind:interval={$settings[nutrient.key].value.interval}
-							limit={$limits[nutrient.key]?.value ? $limits[nutrient.key].value : null}
-							goal={$goals[nutrient.key]?.value ? $goals[nutrient.key].value : null}
-						/>
-					</div>
-				{/each}
-			</div>
+			{#each enabled as nutrient (nutrient.key)}
+				<p
+					class="m-auto flex-[2_1_auto] sm:max-w-full md:max-w-[65%] lg:max-w-[30%]"
+					transition:blur
+					animate:flip={{ duration: 900 }}
+					draggable="true"
+					on:dragstart={dragStartHandler}
+				>
+					<Counter
+						item={nutrient}
+						bind:count={$today[nutrient.key]}
+						bind:interval={$settings[nutrient.key].value.interval}
+						limit={$limits[nutrient.key]?.value ? $limits[nutrient.key].value : null}
+						goal={$goals[nutrient.key]?.value ? $goals[nutrient.key].value : null}
+					/>
+				</p>
+			{/each}
 		{/if}
 	{/await}
-</div>
+</p>
 
 <Add />
