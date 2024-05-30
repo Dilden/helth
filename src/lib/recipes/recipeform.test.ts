c33d1cc210aaaffd3947f7a0d57254448158c49f
dylan <dylan.hildenbrand@gmail.com>
2024-05-30 15:02:58 -0500

    formatting

--- src/lib/recipes/recipeform.test.ts
+++ src/lib/recipes/recipeform.test.ts
@@ -5,131 +5,138 @@ import RecipeForm from './RecipeForm.svelte';
 import { inventoryFilter } from '$stores/stores';
 
 const recipeHasItem1 = {
-  recipe: {
-    id: 10,
-    name: 'noodles',
-    description: 'a lazy meal',
-    items: [
-      {
-        id: 1,
-        name: 'First',
-        description: 'description goes here',
-        servings: 3
-      },
-    ],
-  },
-  inventoryItems: [
-    {
-      id: 1,
-      name: 'First',
-      description: 'description goes here'
-    },
-    {
-      id: 2,
-      name: 'Second',
-      description: 'also goes here'
-    },
-    {
-      id: 3,
-      name: 'third thing',
-      description: 'tis but a test'
-    }
-  ]
-}
-
+	recipe: {
+		id: 10,
+		name: 'noodles',
+		description: 'a lazy meal',
+		items: [
+			{
+				id: 1,
+				name: 'First',
+				description: 'description goes here',
+				servings: 3
+			}
+		]
+	},
+	inventoryItems: [
+		{
+			id: 1,
+			name: 'First',
+			description: 'description goes here'
+		},
+		{
+			id: 2,
+			name: 'Second',
+			description: 'also goes here'
+		},
+		{
+			id: 3,
+			name: 'third thing',
+			description: 'tis but a test'
+		}
+	]
+};
 
 describe('recipe form', () => {
-  it('has text inputs for name and description', async () => {
-    render(RecipeForm);
-    expect(screen.getByLabelText('Recipe Name')).toBeVisible();
-    
-    expect(screen.getByLabelText('Recipe Description')).toBeVisible();
-  })
+	it('has text inputs for name and description', async () => {
+		render(RecipeForm);
+		expect(screen.getByLabelText('Recipe Name')).toBeVisible();
 
-  it('alerts user there are no items in inventory', async () => {
-    render(RecipeForm);
-    expect(screen.getByText('No items found in inventory! Go scan something or Add an Item to your Inventory manually before creating a recipe.')).toBeVisible();
-  })
+		expect(screen.getByLabelText('Recipe Description')).toBeVisible();
+	});
 
-  it('shows inventory items in checkboxes', async () => {
-    render(RecipeForm, {
-      inventoryItems: [
-        {
-          id: 1,
-          name: 'First',
-          description: 'description goes here'
-        },
-        {
-          id: 2,
-          name: 'Second',
-          description: 'also goes here'
-        }
-      ]
-    })
+	it('alerts user there are no items in inventory', async () => {
+		render(RecipeForm);
+		expect(
+			screen.getByText(
+				'No items found in inventory! Go scan something or Add an Item to your Inventory manually before creating a recipe.'
+			)
+		).toBeVisible();
+	});
 
-    expect(screen.getByRole('checkbox', {name: 'First'})).toBeVisible();
-    expect(screen.getByRole('checkbox', {name: 'Second'})).toBeVisible();
-  })
+	it('shows inventory items in checkboxes', async () => {
+		render(RecipeForm, {
+			inventoryItems: [
+				{
+					id: 1,
+					name: 'First',
+					description: 'description goes here'
+				},
+				{
+					id: 2,
+					name: 'Second',
+					description: 'also goes here'
+				}
+			]
+		});
 
-  describe('servings', () => {
-    it('has serving quantity inputs next to checkboxes', () => {
-      render(RecipeForm, recipeHasItem1);
+		expect(screen.getByRole('checkbox', { name: 'First' })).toBeVisible();
+		expect(screen.getByRole('checkbox', { name: 'Second' })).toBeVisible();
+	});
 
-      const boxAncestor: HTMLElement = screen.getByRole('checkbox', {name: 'First'}).closest('span')!; // ! magically tells TS that this won't be null
-      expect(within(boxAncestor).getByLabelText('First')).toBeChecked();
-      expect(within(boxAncestor).getByLabelText('Servings')).toHaveValue(3);
-    })
+	describe('servings', () => {
+		it('has serving quantity inputs next to checkboxes', () => {
+			render(RecipeForm, recipeHasItem1);
 
-    it('hides servings inputs for unchecked boxes', () => {
-      render(RecipeForm, recipeHasItem1);
+			const boxAncestor: HTMLElement = screen
+				.getByRole('checkbox', { name: 'First' })
+				.closest('span')!; // ! magically tells TS that this won't be null
+			expect(within(boxAncestor).getByLabelText('First')).toBeChecked();
+			expect(within(boxAncestor).getByLabelText('Servings')).toHaveValue(3);
+		});
 
-      const boxAncestor: HTMLElement = screen.getByRole('checkbox', {name: 'Second'}).closest('span')!; // ! magically tells TS that this won't be null
-      expect(within(boxAncestor).getByLabelText('Second')).not.toBeChecked();
-      expect(within(boxAncestor).queryByLabelText('Servings')).toBeNull();
-    })
-  })
+		it('hides servings inputs for unchecked boxes', () => {
+			render(RecipeForm, recipeHasItem1);
 
-  it('prefills the fields with data from provided item', () => {
-    render(RecipeForm, recipeHasItem1);
+			const boxAncestor: HTMLElement = screen
+				.getByRole('checkbox', { name: 'Second' })
+				.closest('span')!; // ! magically tells TS that this won't be null
+			expect(within(boxAncestor).getByLabelText('Second')).not.toBeChecked();
+			expect(within(boxAncestor).queryByLabelText('Servings')).toBeNull();
+		});
+	});
 
-    expect(screen.getByLabelText('Recipe Name')).toHaveValue('noodles');
-    expect(screen.getByLabelText('Recipe Description')).toHaveValue('a lazy meal');
-    expect(screen.getByLabelText('First')).toBeChecked();
-    expect(screen.getByLabelText('Second')).not.toBeChecked();
-  })
+	it('prefills the fields with data from provided item', () => {
+		render(RecipeForm, recipeHasItem1);
 
-  it('shows a search box to filter inventory items', () => {
-    render(RecipeForm, {
-      inventoryItems: [
-        {
-          id: 10,
-          name: 'demo',
-          description: 'description goes here'
-        }
-      ]
-    });
-    expect(screen.getByLabelText('Filter inventory')).toBeVisible();
-  })
+		expect(screen.getByLabelText('Recipe Name')).toHaveValue('noodles');
+		expect(screen.getByLabelText('Recipe Description')).toHaveValue('a lazy meal');
+		expect(screen.getByLabelText('First')).toBeChecked();
+		expect(screen.getByLabelText('Second')).not.toBeChecked();
+	});
 
-  it('inventoryFilter text hides elements from inventory', () => {
-    inventoryFilter.set('test');
-    render(RecipeForm, {
-      inventoryItems: [
-        {
-          id: 10,
-          name: 'demo',
-          description: 'description goes here'
-        },
-        {
-          id: 11,
-          name: 'test',
-          description: 'for testing'
-        }
-      ]
-    });
+	it('shows a search box to filter inventory items', () => {
+		render(RecipeForm, {
+			inventoryItems: [
+				{
+					id: 10,
+					name: 'demo',
+					description: 'description goes here'
+				}
+			]
+		});
+		expect(screen.getByLabelText('Filter inventory')).toBeVisible();
+	});
 
-    // parent element is actually hidden
-    expect(screen.getByLabelText('test')).toBeVisible();
-    expect(screen.getByLabelText('demo')).not.toBeVisible();
-  })
-})
+	it('inventoryFilter text hides elements from inventory', () => {
+		inventoryFilter.set('test');
+		render(RecipeForm, {
+			inventoryItems: [
+				{
+					id: 10,
+					name: 'demo',
+					description: 'description goes here'
+				},
+				{
+					id: 11,
+					name: 'test',
+					description: 'for testing'
+				}
+			]
+		});
+
+		// parent element is actually hidden
+		expect(screen.getByLabelText('test')).toBeVisible();
+		expect(screen.getByLabelText('demo')).not.toBeVisible();
+	});
+});
