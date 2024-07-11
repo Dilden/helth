faaa2561e2900b27df594005c0c4be135fde1a43
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2024-07-11 14:59:21 -0500

    Use dateToPicker utility fn from utils/dates

--- src/lib/misc/DatePicker.svelte
+++ src/lib/misc/DatePicker.svelte
@@ -1,13 +1,8 @@
 <script>
+	import { dateToPicker } from '$utils/dates';
 	export let callback = () => {};
 	const dateObj = new Date();
-	const month = dateObj.getMonth() + 1;
-	export let value =
-		dateObj.getFullYear() +
-		'-' +
-		(month.toString().length == 1 ? '0' + month : month) +
-		'-' +
-		dateObj.getDate();
+	export let value = dateToPicker(dateObj);
 </script>
 
 <label for="entry-date">Select date</label>
