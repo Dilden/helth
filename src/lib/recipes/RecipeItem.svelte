f84bc6f64412651cb410f274402d44f14d553764
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2024-08-08 16:36:19 -0500

    Moving delete button around

--- src/lib/recipes/RecipeItem.svelte
+++ src/lib/recipes/RecipeItem.svelte
@@ -74,8 +74,7 @@
 <button
 	class="mx-2 my-0"
 	on:click={addToToday}
-	title="Add Recipe nutients (times specified servings) to Daily Total">➕</button
+	title="Add Recipe nutients (multiplied by specified servings) to Daily Total">➕</button
 >
 <!-- remove from db -->
-<button class="float-right mb-0 ml-2 mt-2" on:click={confirmDelete} title="Delete Recipe">🗑️</button
->
+<button class="float-right m-2 mt-0" on:click={confirmDelete} title="Delete Recipe">🗑️</button>
