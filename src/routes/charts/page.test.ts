25ae1b4ec515cdcbbc4e9792019feaeb5c7f506a
dylan <dylan.hildenbrand@gmail.com>
2025-03-13 15:56:07 -0500

    fixed a broken test!

--- src/routes/charts/page.test.ts
+++ src/routes/charts/page.test.ts
@@ -1,6 +1,7 @@
 import { describe, it, expect, vi, afterEach } from 'vitest';
 import { render, screen } from '@testing-library/svelte';
 import { defaultSettingsStoreValues } from '../../vitest/defaultSettingsStoreValues';
+import { defaultGoalStore, defaultLimitStore } from '../../vitest/defaultGoalLimitStore';
 import Page from './+page.svelte';
 
 describe('history range filters', () => {
@@ -24,24 +25,38 @@ afterEach(() => {
 	vi.restoreAllMocks();
 });
 
+const testGoal1: Goal = {
+	name: 'ayylmfao',
+	value: 69
+};
+vi.mock('$stores/stores.svelte', async () => {
+	return {
+		settings: {
+			set: vi.fn(),
+			init: vi.fn(async () => Promise.resolve(defaultSettingsStoreValues)),
+			remove: vi.fn(),
+			update: vi.fn(),
+			get: vi.fn(() => defaultSettingsStoreValues)
+		},
+		goals: {
+			set: vi.fn(),
+			init: vi.fn(async () => Promise.resolve()),
+			remove: vi.fn(),
+			update: vi.fn(),
+			get: vi.fn(() => defaultGoalStore)
+		},
+		limits: {
+			set: vi.fn(),
+			init: vi.fn(async () => Promise.resolve()),
+			remove: vi.fn(),
+			update: vi.fn(),
+			get: vi.fn(() => defaultLimitStore)
+		}
+	};
+});
 vi.mock('$stores/stores', async () => {
 	const { writable } = await import('svelte/store');
 	return {
-		settings: {
-			...writable(defaultSettingsStoreValues),
-			set: vi.fn(),
-			init: vi.fn()
-		},
-		limits: {
-			...writable([]),
-			set: vi.fn(),
-			init: vi.fn()
-		},
-		goals: {
-			...writable([]),
-			set: vi.fn(),
-			init: vi.fn()
-		},
 		history: {
 			...writable([]),
 			set: vi.fn(),
---
+++ src/vitest/defaultGoalLimitStore.ts
@@ -0,0 +1,28 @@
+export const defaultGoalStore = {
+	added_sugars: {
+		name: 'added_sugars',
+		value: 100
+	},
+	water: {
+		name: 'water',
+		value: 3000
+	},
+	calories: {
+		name: 'calories',
+		value: 1650
+	}
+};
+export const defaultLimitStore = {
+	added_sugars: {
+		name: 'added_sugars',
+		value: 1800
+	},
+	water: {
+		name: 'water',
+		value: 5000
+	},
+	calories: {
+		name: 'calories',
+		value: 1875
+	}
+};
