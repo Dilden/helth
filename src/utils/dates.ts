80113aa7c741c5e5c9fe70f46e313ab4c1c033b5
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2025-04-23 12:49:58 -0500

    thePast should return true for any dates that are equal to it

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -2,7 +2,7 @@ export const thePast = (theDate: Date): Boolean => {
 	const oldDate: Number = new Date(theDate).setHours(0, 0, 0, 0);
 	const currentDate: Number = new Date().setHours(0, 0, 0, 0);
 
-	if (oldDate < currentDate) {
+	if (oldDate <= currentDate) {
 		return true;
 	}
 	return false;
