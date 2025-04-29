86a79316910df2c8a96f889fe81f3180f54ece02
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2025-04-28 21:51:14 -0500

    Fixing lots of little things to get the dexie-cloud sync working and added a lot of tests to keep the focus.

--- src/stores/db.test.ts
+++ src/stores/db.test.ts
@@ -7,7 +7,11 @@ import {
 	addToList,
 	getItemByIdFromTable,
 	updateItemInList,
-	deleteItemFromRecipes
+	deleteItemFromRecipes,
+	findByName,
+	getLatestDay,
+	getDay,
+	updateDay
 } from '$stores/db';
 
 beforeAll(async () => await dbopen);
@@ -16,11 +20,8 @@ afterAll(() => {
 });
 
 describe.sequential('list tables', () => {
-	it('adds an item to a list', async () => {
-		await addToList('recipes', { id: '1', name: 'test', description: 'desc', created: '1' });
-	});
-
 	it('cannot add an item with the same ID', async () => {
+		await addToList('recipes', { id: '1', name: 'test', description: 'desc', created: '1' });
 		await expect(
 			async () =>
 				await addToList('recipes', { id: '1', name: 'should fail', description: 'plz fail' })
@@ -43,12 +44,14 @@ describe.sequential('list tables', () => {
 
 	it('can get an item from a table by id', async () => {
 		const one = await getItemByIdFromTable('recipes', '1');
-		expect(one).toEqual({
-			id: '1',
-			name: 'test',
-			description: 'desc',
-			created: '1'
-		});
+		expect(one).toEqual(
+			expect.objectContaining({
+				id: '1',
+				name: 'test',
+				description: 'desc',
+				created: '1'
+			})
+		);
 	});
 });
 
@@ -88,14 +91,40 @@ it('removes an item from recipes it exists in', async () => {
 	await addToList('inventory', item2);
 	await addToList('recipes', recipe);
 
-	expect(await getItemByIdFromTable('recipes', '30')).toEqual(recipe);
+	expect(await getItemByIdFromTable('recipes', '30')).toEqual(expect.objectContaining(recipe));
 
 	await deleteItemFromRecipes('11');
-	expect(await getItemByIdFromTable('recipes', '30')).toEqual({
-		id: '30',
-		name: 'pizza',
-		description: 'friday nights',
-		items: [{ id: '10' }],
-		created: '0'
+	expect(await getItemByIdFromTable('recipes', '30')).toEqual(
+		expect.objectContaining({
+			id: '30',
+			name: 'pizza',
+			description: 'friday nights',
+			items: [{ id: '10' }],
+			created: '0'
+		})
+	);
+});
+
+describe('settings, goals, limits logic', () => {
+	it('returns an item when given a name + table name', async () => {
+		expect(await findByName('water', 'settings')).toBeTruthy();
+	});
+});
+
+describe.sequential('journal', () => {
+	it('can get a day', async () => {
+		expect(await getLatestDay()).toBeTruthy();
+	});
+	it('can get a day by date', async () => {
+		const x = await getLatestDay();
+		expect(getDay(x?.date)).toBeTruthy();
+	});
+	it('can update a day', async () => {
+		const x = await getLatestDay();
+		expect(await updateDay(x?.date as number, { ...x, calories: 500 })).toEqual(1);
+		expect(await getLatestDay()).toHaveProperty('calories', 500);
+	});
+	it('returns a 0 if there is no date to update', async () => {
+		expect(await updateDay(12, { calories: 500 })).toEqual(0);
 	});
 });
