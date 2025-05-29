14031c80739fa749212fdbf0f4f6d6405904317d
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2025-05-29 15:26:49 -0500

    removing sync/ route, moving logic to settings page

---
+++ src/lib/data/SyncForm.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { db } from '$stores/db';
+
+	let user = db.cloud.currentUser;
+</script>
+
+<div class="flex flex-col gap-2 p-7 md:flex-row">
+	{#if $user.userId !== 'unauthorized'}
+		<p>Logged in as: {$user.name}</p>
+		<button onclick={async () => db.table('$logins').clear()}>Logout</button>
+	{:else}
+		<button onclick={async () => db.cloud.login()}>Login</button>
+	{/if}
+</div>
--- src/lib/data/syncform.test.ts
+++ src/lib/data/syncform.test.ts
@@ -1,11 +1,11 @@
 import { describe, it, expect, vi } from 'vitest';
 import { render, screen } from '@testing-library/svelte';
 import userEvent from '@testing-library/user-event';
-import Page from './+page.svelte';
+import SyncForm from './SyncForm.svelte';
 
 describe('sign in form', () => {
 	it('shows form and input for email', () => {
-		render(Page);
+		render(SyncForm);
 		expect(screen.getByLabelText('Email')).toBeVisible();
 		expect(screen.getByRole('button', { name: 'Submit' })).toBeVisible();
 		expect(screen.getByRole('form')).toBeVisible();
@@ -13,7 +13,7 @@ describe('sign in form', () => {
 	it('emits an event on click', async () => {
 		const user = userEvent.setup();
 
-		render(Page);
+		render(SyncForm);
 		await user.type(screen.getByLabelText('Email'), 'test@test.com');
 		await user.click(screen.getByRole('button', { name: 'Submit' }));
 		expect(screen.getByText('Click the link in your email!')).toBeVisible();
