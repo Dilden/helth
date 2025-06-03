1333f5d6def55b6211abb8b59c11021d7f355a79
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2025-06-02 21:38:15 -0500

    using Dexie cloud login so don't need to check for email or form anymore.

--- src/lib/data/syncform.test.ts
+++ src/lib/data/syncform.test.ts
@@ -1,21 +1,19 @@
 import { describe, it, expect, vi } from 'vitest';
 import { render, screen } from '@testing-library/svelte';
-import userEvent from '@testing-library/user-event';
+// import userEvent from '@testing-library/user-event';
 import SyncForm from './SyncForm.svelte';
 
 describe('sign in form', () => {
-	it('shows form and input for email', () => {
+	it('shows login button', () => {
 		render(SyncForm);
-		expect(screen.getByLabelText('Email')).toBeVisible();
-		expect(screen.getByRole('button', { name: 'Submit' })).toBeVisible();
-		expect(screen.getByRole('form')).toBeVisible();
+		expect(screen.getByRole('button', { name: 'Login' })).toBeVisible();
 	});
-	it('emits an event on click', async () => {
-		const user = userEvent.setup();
+	// it('emits an event on click', async () => {
+	// 	const user = userEvent.setup();
 
-		render(SyncForm);
-		await user.type(screen.getByLabelText('Email'), 'test@test.com');
-		await user.click(screen.getByRole('button', { name: 'Submit' }));
-		expect(screen.getByText('Click the link in your email!')).toBeVisible();
-	});
+	// 	render(SyncForm);
+	// 	await user.type(screen.getByLabelText('Email'), 'test@test.com');
+	// 	await user.click(screen.getByRole('button', { name: 'Submit' }));
+	// 	expect(screen.getByText('Click the link in your email!')).toBeVisible();
+	// });
 });
