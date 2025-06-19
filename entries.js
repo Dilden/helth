97134597739f32e539793cef24bdee050f7795d2
Dylan Hildenbrand <dylan@closingtags.com>
2025-06-19 15:10:49 -0500

    Delete entries.js

--- entries.js
+++
@@ -1,6 +0,0 @@
-import db from './cloud-data.json' with { type: 'json' };
-
-const { data } = db;
-const journal = Object.entries(data['dylan@closingtags.com'].journal);
-
-console.log(journal.length);
