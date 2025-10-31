02d52a591346602052656f336777d00b7dce7bc8
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2025-10-31 13:40:13 -0500

    Attempting to prevent default to confirm before submitting cancellation

--- src/lib/data/CancelForm.svelte
+++ src/lib/data/CancelForm.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { confirmDialog } from '$utils/toast';
+	import { preventDefault } from 'svelte/legacy';
 	interface Props {
 		subscriptionId: string;
 	}
@@ -14,6 +16,15 @@
 				? subscriptionId.substring(1)
 				: subscriptionId}
 		/>
-		<button class="w-auto grow-0 p-2">Cancel Subscription</button>
+		<button
+			class="w-auto grow-0 p-2"
+			onsubmit={preventDefault(() =>
+				confirmDialog(
+					'Are you sure you want to cancel your subscription? Cloud sync will remain active until the end of your current billing cycle',
+					() => console.log('yes'),
+					() => console.log('no')
+				)
+			)}>Cancel Subscription</button
+		>
 	</fieldset>
 </form>
