e016b5a1e033e5487c4d27622365bb832abeb317
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2025-12-09 17:03:23 -0600

    .

--- src/lib/data/cancelform.test.ts
+++ src/lib/data/cancelform.test.ts
@@ -1,8 +1,9 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/svelte';
+import userEvent from '@testing-library/user-event';
 import CancelForm from './CancelForm.svelte';
 
-describe('cancelling a subscription', () => {
+describe('subscription cancelling logic', () => {
 	it('renders a form with subscriptionId in hidden input', () => {
 		render(CancelForm, {
 			subscriptionId: '123456789'
@@ -11,6 +12,7 @@ describe('cancelling a subscription', () => {
 		expect(screen.getByRole('form')).toHaveFormValues({
 			subscriptionId: '123456789'
 		});
+		expect(screen.getByText('Cancel Subscription')).toBeVisible();
 	});
 	it('removes # char from subscriptionId in hidden input', () => {
 		render(CancelForm, {
@@ -21,4 +23,19 @@ describe('cancelling a subscription', () => {
 			subscriptionId: '123456789'
 		});
 	});
+
+	// it('prompts to ensure user wants to cancel', async () => {
+	// 	const user = userEvent.setup();
+	// 	render(CancelForm, {
+	// 		subscriptionId: '#123456789'
+	// 	});
+
+	// 	await user.click(screen.getByRole('button', { name: 'Cancel Subscription' }));
+
+	// 	expect(
+	// 		screen.queryByText(
+	// 			'Are you sure you want to cancel your subscription? Cloud sync will remain active until the end of your current billing cycle.'
+	// 		)
+	// 	).toBeVisible();
+	// });
 });
