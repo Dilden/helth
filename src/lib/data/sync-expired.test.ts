5eb8be2ef087e4d1c88711d0edb37387c84ebdbd
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2025-12-10 14:49:28 -0600

    renaming tests instead of figuring out how to re-mock in each test

---
+++ src/lib/data/sync-expired.test.ts
@@ -0,0 +1,47 @@
+import { describe, it, expect, vi, afterEach } from 'vitest';
+import { render, screen } from '@testing-library/svelte';
+import SyncForm from './SyncForm.svelte';
+
+afterEach(() => {
+	vi.restoreAllMocks();
+});
+
+vi.mock('$stores/db', async () => {
+	const { readable } = await import('svelte/store');
+	return {
+		db: {
+			cloud: {
+				currentUser: {
+					...readable({
+						name: 'Bob',
+						license: {
+							type: 'eval',
+							evalDaysLeft: 0
+						}
+					})
+				},
+				syncState: { ...readable({}) },
+				login: vi.fn(async () => Promise.resolve()),
+				sync: vi.fn(async () => Promise.resolve())
+			},
+			table: vi.fn(() => {
+				return {
+					toArray: vi.fn(() => ['one'])
+				};
+			})
+		}
+	};
+});
+
+describe('sign in form (logged in premium)', () => {
+	it('shows logout button', () => {
+		render(SyncForm);
+		expect(
+			screen.getByText(
+				'Trial period expired! Please purchase a subscription to use cloud sync features.'
+			)
+		).toBeVisible();
+		expect(screen.getByRole('radio', { name: '$1/month' })).toBeVisible();
+		expect(screen.getByRole('radio', { name: '$10/year' })).toBeVisible();
+	});
+});
