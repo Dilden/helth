a1af11b24a012baf83e8adee957eaf8f320b4a9f
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2026-01-26 15:30:37 -0600

    updating tests with necessary scenarios

--- tests/sync.test.ts
+++ tests/sync.test.ts
@@ -32,4 +32,21 @@ test.describe('error handling', () => {
 		});
 		expect(response.status()).toBe(200); // OK
 	});
+	// TODO
+	test.skip('webhook exempt from CSRF', async ({ request }) => {
+		// https://svelte.dev/docs/kit/configuration#csrf
+		//
+		// set trustedOrigins in config to allow Stripe to make POST request
+		// May not be necessary as SvelteKit docs only specify content-type of:
+		// application/x-www-form-urlencoded, multipart/form-data, and text/plain
+		//
+		// but Stripe only sends application/json so maybe not necessary?
+	});
+	test.skip('verifies request came from Stripe', async ({ request }) => {});
+	test.skip('handles duplicate events', async ({ request }) => {});
+	test.skip('listens for subscription renewal', async ({ request }) => {});
+	test.skip('listens for subscription cancel', async ({ request }) => {});
+	test.skip('listens for payment failure', async ({ request }) => {});
+	test.skip('rejects old requests', async ({ request }) => {});
+	test.skip('responds with 200 OK', async ({ request }) => {});
 });
