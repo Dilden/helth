ed94d70961f4ce0583d6b4fa0d55c0ca07dd5174
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2026-01-28 14:27:10 -0600

    setting build --mode to TEST for testing Stripe verification

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	webServer: {
-		command: 'npm run build && npm run preview',
+		command: 'vite build --mode TEST && npm run preview',
 		port: 4173
 	},
 	testDir: 'tests',
