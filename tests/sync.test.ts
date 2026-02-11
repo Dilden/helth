fe347a51a70058a1fc337894959638cb5a7e1819
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2026-02-11 09:48:53 -0600

    Can only test so much w/o Stripe CLI

--- tests/sync.test.ts
+++ tests/sync.test.ts
@@ -1,5 +1,13 @@
 import { expect, test } from '@playwright/test';
 
+// Note: These tests cannot cover all scenarios due to dependency on Stripe's API
+// Further testing must be completed using that CLI tool
+// run
+//
+// stripe listen --forward-to https://localhost:3000/sync --skip-verify
+//
+// to test in local dev env
+
 test.describe('fails non-POST HTTP methods', () => {
 	test('fails a GET request', async ({ request }) => {
 		const response = await request.get('/sync');
@@ -34,17 +42,17 @@ test.describe('error handling', () => {
 			}
 		});
 		expect(response.status()).toBe(200);
-		expect(await response.json()).toStrictEqual({ received: true });
+		expect(await response.json()).toStrictEqual({ received: false });
 	});
 	// TODO
 	// Need to use Stripe CLI to test the rest of these scenarios
 	// since Playwright doesn't support mocking an imported module
-	test.skip('handles duplicate events', async ({ request }) => {});
-	test.skip('listens for subscription renewal', async ({ request }) => {});
-	test.skip('listens for subscription cancel', async ({ request }) => {});
-	test.skip('listens for payment failure', async ({ request }) => {});
-	test.skip('rejects old requests', async ({ request }) => {});
-	test.skip('responds with 200 OK', async ({ request }) => {});
+	test.skip('handles duplicate events', async ({ request }) => { });
+	test.skip('listens for subscription renewal', async ({ request }) => { });
+	test.skip('listens for subscription cancel', async ({ request }) => { });
+	test.skip('listens for payment failure', async ({ request }) => { });
+	test.skip('rejects old requests', async ({ request }) => { });
+	test.skip('responds with 200 OK', async ({ request }) => { });
 	// test.skip('webhook exempt from CSRF', async ({ request }) => {
 	// not needed as SvelteKit doesn't apply CSRF to application/json requests
 	// https://svelte.dev/docs/kit/configuration#csrf
