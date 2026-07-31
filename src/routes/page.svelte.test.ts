feb77b8935ea7f4f11545693dfc067317a1b17e2
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2026-07-31 13:38:40 -0500

    fixing mocks to work with new unified search

--- src/routes/page.svelte.test.ts
+++ src/routes/page.svelte.test.ts
@@ -1,7 +1,7 @@
 import 'fake-indexeddb/auto';
 import { defaultSettingsStoreValues } from '../vitest/defaultSettingsStoreValues';
 import { defaultTodayStoreValues } from '../vitest/defaultTodayStore';
-import { defaultInventory } from '../vitest/defaultInventory';
+import { defaultInventory, defaultRecipes } from '../vitest/defaultInventory';
 import { defaultLimitStore, defaultGoalStore } from '../vitest/defaultGoalLimitStore';
 import { it, expect, vi, afterEach } from 'vitest';
 import { render, screen } from '@testing-library/svelte';
@@ -22,6 +22,7 @@ it('shows counters for all nutrients', async () => {
 
 vi.mock('$stores/stores.svelte', async () => {
 	let s = $state({ query: '' });
+	let q = $state({ query: '' });
 	return {
 		history: {
 			set: vi.fn(),
@@ -68,6 +69,13 @@ vi.mock('$stores/stores.svelte', async () => {
 		inventorySearch: s,
 		subscription: {
 			init: vi.fn(() => Promise.resolve())
-		}
+		},
+		q: q,
+		searchResults: vi.fn(() => {
+			const x = defaultRecipes.map((r) =>
+				r.items.map((i) => defaultInventory.find((y) => y.id == i.id))
+			);
+			return { results: [...defaultInventory, ...x] };
+		})
 	};
 });
